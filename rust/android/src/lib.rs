9a93e8a009caaeff9fd802a9dc21d00631a20ca8
Aditya Kulkarni <adityapk@gmail.com>
2020-05-14 14:20:40 -0700

    Rust SDK build

---
+++ rust/android/src/lib.rs
@@ -0,0 +1,116 @@
+#![cfg(target_os="android")]
+#![allow(non_snake_case)]
+
+#[macro_use] extern crate log;
+extern crate android_logger;
+
+use log::Level;
+use android_logger::Config;
+
+use std::ffi::{CString, CStr};
+use jni::JNIEnv;
+use jni::objects::{JObject, JString};
+use jni::sys::{jstring};
+
+use rustlib;
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_WalletHelper_initlogging(env: JNIEnv, _: JObject) -> jstring {
+    android_logger::init_once(
+        Config::default().with_min_level(Level::Trace),
+    );
+
+    let ok = format!("OK");
+    let output = env.new_string(ok.as_str()).unwrap();
+    return output.into_inner();
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_WalletHelper_initnew(env: JNIEnv, _: JObject, j_serveruri: JString) -> jstring {
+    let server_uri = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_serveruri).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let seed = rustlib::init_new(server_uri);
+
+    let output = env.new_string(seed.as_str()).unwrap();
+    output.into_inner()
+}
+
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_WalletHelper_initfromseed(env: JNIEnv, _: JObject, j_serveruri: JString, j_seed: JString, j_birthday: JString) -> jstring {
+    let server_uri = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_serveruri).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+
+    let seed = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_seed).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let birthday = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_birthday).unwrap().as_ptr()
+        )
+    ).into_string().unwrap().parse::<u64>();
+
+    let seed = rustlib::init_from_seed(server_uri, seed, birthday);
+
+    let output = env.new_string(seed.as_str()).unwrap();
+    output.into_inner()
+}
+
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_WalletHelper_initfromb64(env: JNIEnv, _: JObject, j_serveruri: JString, j_base64: JString) -> jstring {
+    let base64 = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_base64).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let server_uri = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_serveruri).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let seed = rustlib::init_from_b64(server_uri, base64);
+
+    let output = env.new_string(seed.as_str()).unwrap();
+    output.into_inner()
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_WalletHelper_save(env: JNIEnv, _: JObject) -> jstring {
+    let encoded = rustlib::save_to_b64();
+    let output = env.new_string(encoded.as_str()).unwrap();
+    output.into_inner()
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_ysimple2_RPCModule_execute(env: JNIEnv, _: JObject, j_command: JString, j_argslist: JString) -> jstring {
+    let cmd = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_command).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let args_list = CString::from(
+        CStr::from_ptr(
+            env.get_string(j_argslist).unwrap().as_ptr()
+        )
+    ).into_string().unwrap();
+
+    let resp = rustlib::execute(cmd, args_list);
+
+    let output = env.new_string(resp.as_str()).unwrap();
+    output.into_inner()
+}
---
+++ rust/lib/src/lib.rs
@@ -0,0 +1,143 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::{Mutex, Arc};
+use std::cell::RefCell;
+
+use base64::{encode, decode};
+
+use zecwalletlitelib::{commands, lightclient::{LightClient, LightClientConfig}};
+
+// We'll use a MUTEX to store a global lightclient instance,
+// so we don't have to keep creating it. We need to store it here, in rust
+// because we can't return such a complex structure back to JS
+lazy_static! {
+    static ref LIGHTCLIENT: Mutex<RefCell<Option<Arc<LightClient>>>> = Mutex::new(RefCell::new(None));
+}
+pub fn init_new(server_uri: String) -> String {
+    let server = LightClientConfig::get_server_or_default(Some(server_uri));
+    let (config, latest_block_height) = match LightClientConfig::create(server, false) {
+        Ok((c, h)) => (c, h),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let lightclient = match LightClient::new(&config, latest_block_height) {
+        Ok(l) => l,
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let seed = match lightclient.do_seed_phrase() {
+        Ok(s) => s.dump(),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    LIGHTCLIENT.lock().unwrap().replace(Some(Arc::new(lightclient)));
+
+    seed
+}
+
+pub fn init_from_seed(server_uri: String, seed: String, birthday: u64) -> String {
+    let server = LightClientConfig::get_server_or_default(Some(server_uri));
+    let (config, _latest_block_height) = match LightClientConfig::create(server, false) {
+        Ok((c, h)) => (c, h),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let lightclient = match LightClient::new_from_phrase(seed, &config, birthday, false) {
+        Ok(l) => l,
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let seed = match lightclient.do_seed_phrase() {
+        Ok(s) => s.dump(),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    LIGHTCLIENT.lock().unwrap().replace(Some(Arc::new(lightclient)));
+
+    seed
+}
+
+pub fn init_from_b64(server_uri: String, base64_data: String) -> String {
+    let server = LightClientConfig::get_server_or_default(Some(server_uri));
+    let (config, _latest_block_height) = match LightClientConfig::create(server, false) {
+        Ok((c, h)) => (c, h),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let decoded_bytes = decode(&base64_data).unwrap();
+
+    let lightclient = match LightClient::read_from_buffer(&config, &decoded_bytes[..]) {
+        Ok(l) => l,
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    let seed = match lightclient.do_seed_phrase() {
+        Ok(s) => s.dump(),
+        Err(e) => {
+            return format!("Error: {}", e);
+        }
+    };
+
+    LIGHTCLIENT.lock().unwrap().replace(Some(Arc::new(lightclient)));
+
+    seed
+}
+
+pub fn save_to_b64() -> String {
+    // Return the wallet as a base64 encoded string
+    let lightclient: Arc<LightClient>;
+    {
+        let lc = LIGHTCLIENT.lock().unwrap();
+
+        if lc.borrow().is_none() {
+            return format!("Error: Light Client is not initialized");
+        }
+
+        lightclient = lc.borrow().as_ref().unwrap().clone();
+    };
+
+    match lightclient.do_save_to_buffer() {
+        Ok(buf) => encode(&buf),
+        Err(e) => {
+            format!("Error: {}", e)
+        }
+    }
+}
+
+pub fn execute(cmd: String, args_list: String) -> String {
+    let resp: String;
+    {
+        let lightclient: Arc<LightClient>;
+        {
+            let lc = LIGHTCLIENT.lock().unwrap();
+
+            if lc.borrow().is_none() {
+                return format!("Error: Light Client is not initialized");
+            }
+
+            lightclient = lc.borrow().as_ref().unwrap().clone();
+        };
+
+        let args = if args_list.is_empty() { vec![] } else { vec![args_list.as_ref()] };
+        resp = commands::do_user_command(&cmd, &args, lightclient.as_ref()).clone();
+    };
+
+    resp
+}
