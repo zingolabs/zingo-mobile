c9a3a67a29d28948db02378ce7b09343bcd9017a
Aditya Kulkarni <adityapk@gmail.com>
2020-06-01 10:35:30 -0700

    Android app store publish build number

--- components/About.tsx
+++ components/About.tsx
@@ -26,7 +26,7 @@ const AboutModal: React.FunctionComponent<AboutModalProps> = ({closeModal}) => {
           justifyContent: 'flex-start',
           margin: 10,
         }}>
-        <BoldText>Zecwallet Lite v0.1-beta2</BoldText>
+        <BoldText>Zecwallet Lite v1.0</BoldText>
 
         <RegText>
           {'\n'}
