a4951c6b4224e93daa8a225543a3e06ebe96db4f
Aditya Kulkarni <adityapk@gmail.com>
2020-06-10 11:39:48 -0700

    v1.0.9 android release

--- components/About.tsx
+++ components/About.tsx
@@ -26,7 +26,7 @@ const AboutModal: React.FunctionComponent<AboutModalProps> = ({closeModal}) => {
           justifyContent: 'flex-start',
           margin: 10,
         }}>
-        <BoldText>Zecwallet Lite v1.0.7</BoldText>
+        <BoldText>Zecwallet Lite v1.0.9</BoldText>
 
         <RegText>
           {'\n'}
