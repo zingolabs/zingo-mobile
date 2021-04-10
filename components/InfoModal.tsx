5fef58333e059abcf614c0f8d71da56966f136e9
Aditya Kulkarni <adityapk@gmail.com>
2021-04-09 20:06:22 -0700

    v1.4.6

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -57,7 +57,7 @@ const InfoModal: React.FunctionComponent<InfoModalProps> = ({info, walletHeight,
         </View>
 
         <View style={{display: 'flex', margin: 10}}>
-          <DetailLine label="Wallet Version" value="Zecwallet Lite v1.4.5" />
+          <DetailLine label="Wallet Version" value="Zecwallet Lite v1.4.6" />
           <DetailLine label="Server Version" value={info?.version} />
           <DetailLine label="Lightwallet Server URL" value={info?.serverUri} />
           <DetailLine label="Network" value={info?.testnet ? 'Testnet' : 'Mainnet'} />
