1d633c58ea20b05e8750b952a41da4dae886eaed
Gunnar-Stunnar <gjenserro@gmail.com>
2021-12-23 02:49:33 -0700

    Fixed docker files build, updated the docker images and included a new JDK,NDK docker image

--- rust/android/build.sh
+++ rust/android/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
+# docker build --tag stunnar/rustndk:latest rustNDKDocker
 docker build --tag zecwalletmobile/android:latest docker
 
 docker run --rm -v $(pwd)/..:/opt/zecwalletmobile -v $(pwd)/target/registry:/root/.cargo/registry zecwalletmobile/android:latest bash -c "
     cd /opt/zecwalletmobile/android && \
-    CC=i686-linux-android24-clang OPENSSL_DIR=/opt/openssl-1.1.1e/x86 cargo build --target i686-linux-android --release && i686-linux-android-strip ../target/i686-linux-android/release/librust.so && \
-    CC=armv7a-linux-androideabi24-clang OPENSSL_DIR=/opt/openssl-1.1.1e/armv7 cargo build --target armv7-linux-androideabi --release && arm-linux-androideabi-strip ../target/armv7-linux-androideabi/release/librust.so && \
-    CC=aarch64-linux-android24-clang OPENSSL_DIR=/opt/openssl-1.1.1e/aarch64 cargo build --target aarch64-linux-android --release && aarch64-linux-android-strip ../target/aarch64-linux-android/release/librust.so"
+    AR=llvm-ar LD=ld RANLIB=llvm-ranlib CC=i686-linux-android29-clang OPENSSL_DIR=/opt/openssl-3.0.1/x86 cargo +nightly build -Z build-std --target i686-linux-android --release && llvm-strip ../target/i686-linux-android/release/librust.so && \
+    AR=llvm-ar LD=ld RANLIB=llvm-ranlib CC=armv7a-linux-androideabi29-clang OPENSSL_DIR=/opt/openssl-3.0.1/armv7 cargo +nightly build  -Z build-std --target armv7-linux-androideabi --release && llvm-strip ../target/armv7-linux-androideabi/release/librust.so && \
+    AR=llvm-ar LD=ld RANLIB=llvm-ranlib CC=aarch64-linux-android29-clang OPENSSL_DIR=/opt/openssl-3.0.1/aarch64 cargo  +nightly build   -Z build-std --target aarch64-linux-android --release && llvm-strip ../target/aarch64-linux-android/release/librust.so"
+
+# docker run --rm -v $(pwd)/..:/opt/zecwalletmobile -v $(pwd)/target/registry:/root/.cargo/registry -v $(pwd):/app -w /app zecwalletmobile/android:latest bash -c "
+#      CC=i686-linux-android24-clang OPENSSL_DIR=/opt/openssl-3.0.1/x86 cargo ndk -t x86 -o ./jniLibs build
+#      "
 
 mkdir -p ../../android/app/src/main/jniLibs/arm64-v8a
 mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7a
