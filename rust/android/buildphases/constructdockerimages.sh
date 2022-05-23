5f7f6a2e037df7cbb27a1ee50ae9c3ce116eb9c4
dannasessha <sessha@zingolabs.com>
2022-05-23 23:10:28 +0000

    add user and stage to Dockerfile, adjust build script

--- rust/android/buildphases/constructdockerimages.sh
+++ rust/android/buildphases/constructdockerimages.sh
@@ -2,4 +2,7 @@
 
 # build docker images
 docker build --target apt_upgrade_rustndk_image . \
-&& docker build --target rustndk_openssl_installed_image --tag devlocal/rustndk_openssl_installed_image .
+&& docker build --target rustndk_openssl_installed_image --tag devlocal/rustndk_openssl_installed_image \
+ --build-arg UID=`id -u` \
+ --build-arg GUID=`id -g` \
+ .
