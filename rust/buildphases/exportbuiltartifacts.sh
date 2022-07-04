eeb6c61d40518cf2056962b90d834eaafb452ac1
Skylar Saveland <skylar.saveland@gmail.com>
2022-07-04 03:39:18 -0700

    so close

--- rust/buildphases/exportbuiltartifacts.sh
+++ rust/buildphases/exportbuiltartifacts.sh
@@ -6,18 +6,22 @@ mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7a
 mkdir -p ../../android/app/src/main/jniLibs/x86
 mkdir -p ../../android/app/src/main/jniLibs/x86_64
 
+id=$(docker create devlocal/rustndk_openssl_installed_image)
+
 docker cp \
-    rustndk_openssl_installed_image:/opt/zingo/rust/target/x86_64-linux-android/release/librust.so \
+    $id:/opt/zingo/rust/target/x86_64-linux-android/release/librust.so \
     ../../android/app/src/main/jniLibs/x86_64/librust.so
 docker cp \
-    rustndk_openssl_installed_image:/opt/zingo/rust/target/i686-linux-android/release/librust.so \
+    $id:/opt/zingo/rust/target/i686-linux-android/release/librust.so \
     ../../android/app/src/main/jniLibs/x86/librust.so
 docker cp \
-    rustndk_openssl_installed_image:/opt/zingo/rust/target/armv7-linux-androideabi/release/librust.so \
+    $id:/opt/zingo/rust/target/armv7-linux-androideabi/release/librust.so \
     ../../android/app/src/main/jniLibs/armeabi-v7a/librust.so
 docker cp \
-    rustndk_openssl_installed_image:/opt/zingo/rust/target/aarch64-linux-android/release/librust.so \
+    $id:/opt/zingo/rust/target/aarch64-linux-android/release/librust.so \
     ../../android/app/src/main/jniLibs/arm64-v8a/librust.so
+
+docker rm -v $id
 
 # cp ../target/x86_64-linux-android/release/librust.so   ../../android/app/src/main/jniLibs/x86_64/
 # cp ../target/i686-linux-android/release/librust.so   ../../android/app/src/main/jniLibs/x86/
