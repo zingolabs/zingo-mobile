93c78f6365f0c1758442872aba3ead8f574dff0a
JC <juanky201271@gmail.com>
2022-10-18 23:27:00 -0600

    Android and IOS v44

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -70,7 +70,7 @@ const InfoModal: React.FunctionComponent<InfoModalProps> = ({info, closeModal, t
           justifyContent: 'flex-start',
         }}>
         <View style={{display: 'flex', margin: 20}}>
-          <DetailLine label="Version" value="Zingo! v0.0.1_42" />
+          <DetailLine label="Version" value="Zingo! v0.0.1_44" />
           <DetailLine label="Server Version" value={infoState.version ? infoState.version : '...loading...'} />
           <DetailLine label="Lightwalletd URL" value={infoState.serverUri ? infoState.serverUri : '...loading...'} />
           <DetailLine
