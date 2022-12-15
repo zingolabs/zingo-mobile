844d57ab6e23ace08e1449bb987855a9d5c52593
JC <juanky201271@gmail.com>
2022-12-15 09:47:13 -0700

    about snapshot test refactored

--- __tests__/About.snapshot.tsx
+++ __tests__/About.snapshot.tsx
@@ -6,15 +6,40 @@ import 'react-native';
 import React from 'react';
 
 import { create } from 'react-test-renderer';
-import AboutModal from '../components/About';
+import About from '../components/About';
+import { ContextLoadedProvider } from '../app/context';
 
+jest.mock('react-native-localize', () => ({
+  getNumberFormatSettings: () => {
+    return {
+      decimalSeparator: '.',
+      groupingSeparator: ',',
+    };
+  },
+}));
 jest.useFakeTimers();
 
 // test suite
 describe('Component AboutModal - test', () => {
   //snapshot test
   test('AboutModal - snapshot', () => {
-    const aboutModal = create(<AboutModal closeModal={() => {}} />);
-    expect(aboutModal.toJSON()).toMatchSnapshot();
+    const state = {
+      translate: () => [
+        '1 text translated line 1',
+        '2 text translated line 2',
+        '3 text translated line 3',
+        '4 text translated line 4',
+        '5 text translated line 5',
+      ],
+      totalBalance: {
+        total: 0,
+      },
+    };
+    const about = create(
+      <ContextLoadedProvider value={state}>
+        <About closeModal={() => {}} />
+      </ContextLoadedProvider>,
+    );
+    expect(about.toJSON()).toMatchSnapshot();
   });
 });
