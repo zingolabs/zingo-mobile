5d9627e43a8f0eb435313ef19e6e2efb975f4cc6
JC <juanky201271@gmail.com>
2022-12-16 09:07:13 -0700

    Info es/us unit tests complete & working

--- __tests__/Info.es.unit.tsx
+++ __tests__/Info.es.unit.tsx
@@ -11,6 +11,7 @@ import { ContextLoadedProvider } from '../app/context';
 
 import {
   ErrorModalData,
+  InfoType,
   ReceivePageState,
   SendPageState,
   SendProgress,
@@ -37,67 +38,69 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 // test suite
 describe('Component Info - test', () => {
   //unit test
-  const state = {
-    navigation: null,
-    route: null,
-    dimensions: {} as {
-      width: number;
-      height: number;
-      orientation: 'portrait' | 'landscape';
-      deviceType: 'tablet' | 'phone';
-      scale: number;
-    },
-
-    syncStatusReport: new SyncStatusReport(),
-    addressPrivateKeys: new Map(),
-    addresses: [],
-    addressBook: [],
-    transactions: null,
-    sendPageState: new SendPageState(new ToAddr(0)),
-    receivePageState: new ReceivePageState(),
-    rescanning: false,
-    wallet_settings: new WalletSettings(),
-    syncingStatus: null,
-    errorModalData: new ErrorModalData(),
-    txBuildProgress: new SendProgress(),
-    walletSeed: null,
-    isMenuDrawerOpen: false,
-    selectedMenuDrawerItem: '',
-    aboutModalVisible: false,
-    computingModalVisible: false,
-    settingsModalVisible: false,
-    infoModalVisible: false,
-    rescanModalVisible: false,
-    seedViewModalVisible: false,
-    seedChangeModalVisible: false,
-    seedBackupModalVisible: false,
-    seedServerModalVisible: false,
-    syncReportModalVisible: false,
-    poolsModalVisible: false,
-    newServer: null,
-    uaAddress: null,
-    info: {
-      testnet: false,
-      serverUri: 'serverUri',
-      latestBlock: 0,
-      connections: 0,
-      version: '0',
-      verificationProgress: 0,
-      currencyName: 'ZEC',
-      solps: 0,
-      zecPrice: 33.33,
-      defaultFee: 0,
-      encrypted: false,
-      locked: false,
-      chain_name: '',
-    },
-    translate: () => 'translated text',
-    totalBalance: new TotalBalance(),
-  };
   test('Info - price with es (,) decimal point', () => {
+    const state = {
+      navigation: null,
+      route: null,
+      dimensions: {} as {
+        width: number;
+        height: number;
+        orientation: 'portrait' | 'landscape';
+        deviceType: 'tablet' | 'phone';
+        scale: number;
+      },
+
+      syncStatusReport: new SyncStatusReport(),
+      addressPrivateKeys: new Map(),
+      addresses: [],
+      addressBook: [],
+      transactions: null,
+      sendPageState: new SendPageState(new ToAddr(0)),
+      receivePageState: new ReceivePageState(),
+      rescanning: false,
+      wallet_settings: new WalletSettings(),
+      syncingStatus: null,
+      errorModalData: new ErrorModalData(),
+      txBuildProgress: new SendProgress(),
+      walletSeed: null,
+      isMenuDrawerOpen: false,
+      selectedMenuDrawerItem: '',
+      aboutModalVisible: false,
+      computingModalVisible: false,
+      settingsModalVisible: false,
+      infoModalVisible: false,
+      rescanModalVisible: false,
+      seedViewModalVisible: false,
+      seedChangeModalVisible: false,
+      seedBackupModalVisible: false,
+      seedServerModalVisible: false,
+      syncReportModalVisible: false,
+      poolsModalVisible: false,
+      newServer: null,
+      uaAddress: null,
+      info: {
+        testnet: false,
+        serverUri: 'https://zcash.es',
+        latestBlock: 2000100,
+        connections: 0,
+        version: '3.3.3.0',
+        verificationProgress: 0,
+        currencyName: 'ZEC',
+        solps: 0,
+        zecPrice: 33.33,
+        defaultFee: 1000,
+        encrypted: false,
+        locked: false,
+        chain_name: 'mainnet',
+      } as InfoType,
+      translate: () => 'translated text',
+      totalBalance: new TotalBalance(),
+    };
+    state.totalBalance.total = 1.12345678;
+    const onClose = jest.fn();
     const text: any = render(
       <ContextLoadedProvider value={state}>
-        <Info closeModal={() => {}} />
+        <Info closeModal={onClose} />
       </ContextLoadedProvider>,
     ).toJSON();
     expect(text.type).toBe('RCTSafeAreaView');
