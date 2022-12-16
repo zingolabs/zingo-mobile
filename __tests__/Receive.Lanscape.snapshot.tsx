7e9dcc9cadfa60f72b1ca2d48601536cacb7ef17
JC <juanky201271@gmail.com>
2022-12-16 10:27:16 -0700

    Receive snapshot tests complete & working

--- __tests__/Receive.Lanscape.snapshot.tsx
+++ __tests__/Receive.Lanscape.snapshot.tsx
@@ -11,6 +11,7 @@ import { ContextLoadedProvider } from '../app/context';
 
 import {
   ErrorModalData,
+  InfoType,
   ReceivePageState,
   SendPageState,
   SendProgress,
@@ -42,7 +43,7 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 // test suite
 describe('Component Receive - test', () => {
   //snapshot test
-  test('Receive - snapshot', () => {
+  test('Receive Landscape - snapshot', () => {
     const state = {
       navigation: null,
       route: null,
@@ -53,7 +54,7 @@ describe('Component Receive - test', () => {
       transactions: null,
       sendPageState: new SendPageState(new ToAddr(0)),
       receivePageState: new ReceivePageState(),
-      info: null,
+      info: {} as InfoType,
       rescanning: false,
       wallet_settings: new WalletSettings(),
       syncingStatus: null,
@@ -74,23 +75,37 @@ describe('Component Receive - test', () => {
       syncReportModalVisible: false,
       poolsModalVisible: false,
       newServer: null,
-      uaAddress: 'UA',
+      uaAddress: 'UA-12345678901234567890',
       addresses: [
         {
-          uaAddress: 'UA',
-          address: 'UA',
+          uaAddress: 'UA-12345678901234567890',
+          address: 'UA-12345678901234567890',
           addressKind: 'u',
           containsPending: false,
           receivers: 'ozt',
         },
+        {
+          uaAddress: 'UA-12345678901234567890',
+          address: 'sapling-12345678901234567890',
+          addressKind: 'z',
+          containsPending: false,
+          receivers: 'z',
+        },
+        {
+          uaAddress: 'UA-12345678901234567890',
+          address: 'transparent-12345678901234567890',
+          addressKind: 't',
+          containsPending: false,
+          receivers: 't',
+        },
       ],
       translate: () => 'text translated',
       dimensions: {
-        width: 200,
-        height: 200,
-        orientation: 'portrait',
-        deviceType: 'tablet',
-        scale: 1.5,
+        width: 600,
+        height: 300,
+        orientation: 'landscape',
+        deviceType: 'phone',
+        scale: 2.5,
       } as {
         width: number;
         height: number;
@@ -100,15 +115,18 @@ describe('Component Receive - test', () => {
       },
       totalBalance: new TotalBalance(),
     };
+    state.info.currencyName = 'ZEC';
+    state.totalBalance.total = 1.12345678;
+    const onFunction = jest.fn();
     const receive = render(
       <ContextLoadedProvider value={state}>
         <Receive
-          fetchTotalBalance={() => {}}
-          setUaAddress={() => {}}
-          toggleMenuDrawer={() => {}}
-          startRescan={() => {}}
-          syncingStatusMoreInfoOnClick={() => {}}
-          poolsMoreInfoOnClick={() => {}}
+          fetchTotalBalance={onFunction}
+          setUaAddress={onFunction}
+          toggleMenuDrawer={onFunction}
+          startRescan={onFunction}
+          syncingStatusMoreInfoOnClick={onFunction}
+          poolsMoreInfoOnClick={onFunction}
         />
       </ContextLoadedProvider>,
     );
---
+++ __tests__/Receive.Portrait.snapshot.tsx
@@ -0,0 +1,135 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+import { render } from '@testing-library/react-native';
+import Receive from '../components/Receive';
+import { ContextLoadedProvider } from '../app/context';
+
+import {
+  ErrorModalData,
+  InfoType,
+  ReceivePageState,
+  SendPageState,
+  SendProgress,
+  SyncStatusReport,
+  ToAddr,
+  TotalBalance,
+  WalletSettings,
+} from '../app/AppState';
+
+jest.useFakeTimers();
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+  FontAwesomeIcon: '',
+}));
+jest.mock('react-native-localize', () => ({
+  getNumberFormatSettings: () => {
+    return {
+      decimalSeparator: '.',
+      groupingSeparator: ',',
+    };
+  },
+}));
+jest.mock('react-native-tab-view', () => ({
+  TabView: '',
+  TabBar: '',
+}));
+jest.mock('react-native-option-menu', () => '');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+// test suite
+describe('Component Receive - test', () => {
+  //snapshot test
+  test('Receive Portrait - snapshot', () => {
+    const state = {
+      navigation: null,
+      route: null,
+
+      syncStatusReport: new SyncStatusReport(),
+      addressPrivateKeys: new Map(),
+      addressBook: [],
+      transactions: null,
+      sendPageState: new SendPageState(new ToAddr(0)),
+      receivePageState: new ReceivePageState(),
+      info: {} as InfoType,
+      rescanning: false,
+      wallet_settings: new WalletSettings(),
+      syncingStatus: null,
+      errorModalData: new ErrorModalData(),
+      txBuildProgress: new SendProgress(),
+      walletSeed: null,
+      isMenuDrawerOpen: false,
+      selectedMenuDrawerItem: '',
+      aboutModalVisible: false,
+      computingModalVisible: false,
+      settingsModalVisible: false,
+      infoModalVisible: false,
+      rescanModalVisible: false,
+      seedViewModalVisible: false,
+      seedChangeModalVisible: false,
+      seedBackupModalVisible: false,
+      seedServerModalVisible: false,
+      syncReportModalVisible: false,
+      poolsModalVisible: false,
+      newServer: null,
+      uaAddress: 'UA-12345678901234567890',
+      addresses: [
+        {
+          uaAddress: 'UA-12345678901234567890',
+          address: 'UA-12345678901234567890',
+          addressKind: 'u',
+          containsPending: false,
+          receivers: 'ozt',
+        },
+        {
+          uaAddress: 'UA-12345678901234567890',
+          address: 'sapling-12345678901234567890',
+          addressKind: 'z',
+          containsPending: false,
+          receivers: 'z',
+        },
+        {
+          uaAddress: 'UA-12345678901234567890',
+          address: 'transparent-12345678901234567890',
+          addressKind: 't',
+          containsPending: false,
+          receivers: 't',
+        },
+      ],
+      translate: () => 'text translated',
+      dimensions: {
+        width: 200,
+        height: 400,
+        orientation: 'portrait',
+        deviceType: 'tablet',
+        scale: 1.5,
+      } as {
+        width: number;
+        height: number;
+        orientation: 'portrait' | 'landscape';
+        deviceType: 'tablet' | 'phone';
+        scale: number;
+      },
+      totalBalance: new TotalBalance(),
+    };
+    state.info.currencyName = 'ZEC';
+    state.totalBalance.total = 1.12345678;
+    const onFunction = jest.fn();
+    const receive = render(
+      <ContextLoadedProvider value={state}>
+        <Receive
+          fetchTotalBalance={onFunction}
+          setUaAddress={onFunction}
+          toggleMenuDrawer={onFunction}
+          startRescan={onFunction}
+          syncingStatusMoreInfoOnClick={onFunction}
+          poolsMoreInfoOnClick={onFunction}
+        />
+      </ContextLoadedProvider>,
+    );
+    expect(receive.toJSON()).toMatchSnapshot();
+  });
+});
