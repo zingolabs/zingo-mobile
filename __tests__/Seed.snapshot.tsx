47a6d355c810423392da2148d4b7fa394bdc245c
JC <juanky201271@gmail.com>
2022-12-16 11:03:03 -0700

    Seed snapshot test complete & working

--- __tests__/Seed.snapshot.tsx
+++ __tests__/Seed.snapshot.tsx
@@ -7,16 +7,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react-native';
 import Seed from '../components/Seed';
-import { ContextLoadedProvider } from '../app/context';
+import { ContextLoadedProvider, ContextLoadingProvider } from '../app/context';
 
 import {
   ErrorModalData,
+  InfoType,
   ReceivePageState,
   SendPageState,
   SendProgress,
   SyncStatusReport,
   ToAddr,
   TotalBalance,
+  WalletSeed,
   WalletSettings,
 } from '../app/AppState';
 
@@ -37,7 +39,7 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 // test suite
 describe('Component Seed - test', () => {
   //snapshot test
-  const state = {
+  const stateLoaded = {
     navigation: null,
     route: null,
     dimensions: {} as {
@@ -55,7 +57,7 @@ describe('Component Seed - test', () => {
     transactions: null,
     sendPageState: new SendPageState(new ToAddr(0)),
     receivePageState: new ReceivePageState(),
-    info: null,
+    info: {} as InfoType,
     rescanning: false,
     wallet_settings: new WalletSettings(),
     syncingStatus: null,
@@ -92,15 +94,97 @@ describe('Component Seed - test', () => {
     },
     totalBalance: new TotalBalance(),
     walletSeed: {
-      seed: 'pepe lolo',
-      birthday: 0,
-    },
+      seed: 'pepe lolo titi',
+      birthday: 1500100,
+    } as WalletSeed,
   };
-  test('Seed - snapshot', () => {
+  stateLoaded.info.currencyName = 'ZEC';
+  stateLoaded.totalBalance.total = 1.12345678;
+  const onOk = jest.fn();
+  const onCancel = jest.fn();
+  test('Seed View - snapshot', () => {
     const seed = render(
-      <ContextLoadedProvider value={state}>
-        <Seed onClickOK={() => {}} onClickCancel={() => {}} action={'view'} />
+      <ContextLoadedProvider value={stateLoaded}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'view'} />
       </ContextLoadedProvider>,
+    );
+    expect(seed.toJSON()).toMatchSnapshot();
+  });
+  test('Seed Change - snapshot', () => {
+    const seed = render(
+      <ContextLoadedProvider value={stateLoaded}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'change'} />
+      </ContextLoadedProvider>,
+    );
+    expect(seed.toJSON()).toMatchSnapshot();
+  });
+  test('Seed Server - snapshot', () => {
+    const seed = render(
+      <ContextLoadedProvider value={stateLoaded}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'server'} />
+      </ContextLoadedProvider>,
+    );
+    expect(seed.toJSON()).toMatchSnapshot();
+  });
+  test('Seed Backup - snapshot', () => {
+    const seed = render(
+      <ContextLoadedProvider value={stateLoaded}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'backup'} />
+      </ContextLoadedProvider>,
+    );
+    expect(seed.toJSON()).toMatchSnapshot();
+  });
+  const stateLoading = {
+    navigation: null,
+    route: null,
+    dimensions: {} as {
+      width: number;
+      height: number;
+      orientation: 'portrait' | 'landscape';
+      deviceType: 'tablet' | 'phone';
+      scale: number;
+    },
+
+    screen: 0,
+    actionButtonsDisabled: false,
+    walletExists: false,
+    server: null,
+    info: null,
+
+    translate: (p: string) => {
+      if (p === 'seed.buttontexts') {
+        return {
+          new: [''],
+          change: [''],
+          server: [''],
+          view: [''],
+          restore: [''],
+          backup: [''],
+        };
+      } else {
+        return 'text translated';
+      }
+    },
+    totalBalance: new TotalBalance(),
+    walletSeed: {
+      seed: 'pepe lolo titi',
+      birthday: 1500100,
+    } as WalletSeed,
+  };
+  stateLoading.totalBalance.total = 1.12345678;
+  test('Seed New - snapshot', () => {
+    const seed = render(
+      <ContextLoadingProvider value={stateLoading}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'new'} />
+      </ContextLoadingProvider>,
+    );
+    expect(seed.toJSON()).toMatchSnapshot();
+  });
+  test('Seed Restore - snapshot', () => {
+    const seed = render(
+      <ContextLoadingProvider value={stateLoading}>
+        <Seed onClickOK={onOk} onClickCancel={onCancel} action={'restore'} />
+      </ContextLoadingProvider>,
     );
     expect(seed.toJSON()).toMatchSnapshot();
   });
