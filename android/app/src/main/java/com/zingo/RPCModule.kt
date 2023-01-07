66879b29bfdd6ddde99b9d69759974d8438c5387
JC <juanky201271@gmail.com>
2023-01-07 15:16:41 -0700

    New init_light_client feature working in Android

--- android/app/src/main/java/com/zingo/RPCModule.kt
+++ android/app/src/main/java/com/zingo/RPCModule.kt
@@ -25,6 +25,7 @@ class RPCModule internal constructor(private val reactContext: ReactApplicationC
     private external fun initfromseed(serveruri: String, seed: String, birthday: String, saplingOutputb64: String, saplingSpendb64: String, datadir: String): String
     private external fun initfromb64(serveruri: String, datab64: String, saplingOutputb64: String, saplingSpendb64: String, datadir: String): String
     private external fun save(): String
+    private external fun initlightclient(serveruri: String, saplingOutputb64: String, saplingSpendb64: String, datadir: String): String
 
     override fun getName(): String {
         return "RPCModule"
@@ -494,6 +495,87 @@ class RPCModule internal constructor(private val reactContext: ReactApplicationC
         } catch (e: IllegalArgumentException) {
             // Log.e("MAIN", "Couldn't save the wallet backup")
         }
+    }
+
+    @ReactMethod
+    fun initLightClient(server: String, promise: Promise) {
+        // Log.w("MAIN", "Initialize Light Client")
+
+        val saplingSpendFile: InputStream = MainApplication.getAppContext()?.resources?.openRawResource(R.raw.saplingspend)!!
+        var saplingSpend = saplingSpendFile.readBytes()
+        saplingSpendFile.close()
+
+        val middle0 =        0
+        val middle1 =  6000000 // 6_000_000 - 8 pieces
+        val middle2 = 12000000
+        val middle3 = 18000000
+        val middle4 = 24000000
+        val middle5 = 30000000
+        val middle6 = 36000000
+        val middle7 = 42000000
+        val middle8: Int = saplingSpend.size
+        var saplingSpendEncoded = StringBuilder(Base64.encodeToString(saplingSpend, middle0, middle1 - middle0, Base64.NO_WRAP))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle1,
+            middle2 - middle1,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle2,
+            middle3 - middle2,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle3,
+            middle4 - middle3,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle4,
+            middle5 - middle4,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle5,
+            middle6 - middle5,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle6,
+            middle7 - middle6,
+            Base64.NO_WRAP
+        ))
+        saplingSpendEncoded = saplingSpendEncoded.append(Base64.encodeToString(
+            saplingSpend,
+            middle7,
+            middle8 - middle7,
+            Base64.NO_WRAP
+        ))
+        saplingSpend = ByteArray(0)
+
+        val saplingOutputFile: InputStream = MainApplication.getAppContext()?.resources?.openRawResource(R.raw.saplingoutput)!!
+        var saplingOutput = saplingOutputFile.readBytes()
+        saplingOutputFile.close()
+
+        val saplingOutputEncoded = StringBuilder(Base64.encodeToString(saplingOutput, Base64.NO_WRAP))
+
+        saplingOutput = ByteArray(0)
+
+        initlogging()
+
+        // Initialize Light Client
+        val resp = initlightclient(server,
+            saplingOutputEncoded.toString(),
+            saplingSpendEncoded.toString(),
+            reactContext.applicationContext.filesDir.absolutePath)
+
+        promise.resolve(resp)
     }
 
 }
