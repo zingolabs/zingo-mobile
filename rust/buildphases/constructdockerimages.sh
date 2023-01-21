bcad84cdbc2f9efe546f1b187a90d4f277c4a401
Oscar Pepper <oscarpepper@zingolabs.org>
2023-01-21 16:11:30 -0600

    android build from builder image - dockerhub

--- rust/buildphases/constructdockerimages.sh
+++ rust/buildphases/constructdockerimages.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/env bash
 
-# build docker images
-docker build --target apt_upgrade_rustndk_image . \
-&& docker build --target rustndk_openssl_installed_image --tag devlocal/rustndk_openssl_installed_image \
+# build docker image
+docker build --target build_android --tag devlocal/build_android \
  --build-arg UID=`id -u` \
  --build-arg GUID=`id -g` \
  .
