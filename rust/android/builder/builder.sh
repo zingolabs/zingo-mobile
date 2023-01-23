09f48f30df89a66261ba9c51e2ddf02fe2561a32
Oscar Pepper <oscarpepper@zingolabs.org>
2023-01-23 09:35:00 -0600

    removed unused UID and GUID arguments from android build

--- rust/android/builder/builder.sh
+++ rust/android/builder/builder.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 # android builder image
+# upload updated builder images to zingodevops dockerhub
 docker build --target apt_upgrade_rustndk_image . \
 && docker build --target rustndk_openssl_installed_image --tag zingodevops/android_builder \
  --build-arg UID=`id -u` \
--- rust/buildphases/constructdockerimages.sh
+++ rust/buildphases/constructdockerimages.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
 
 # build docker image
-docker build --target build_android --tag devlocal/build_android \
- --build-arg UID=`id -u` \
- --build-arg GUID=`id -g` \
- .
+docker build --target build_android --tag devlocal/build_android .
