fe5d576f7d3f103c4604f7703c84cbe4a0859384
fluidvanadium <fluidvanadium@zingolabs.org>
2023-02-02 17:49:35 +0000

    clean tests

--- e2e/my.test.js
+++ e2e/my.test.js
@@ -19,14 +19,20 @@ describe('Example', () => {
   //   // log.error(device.appLaunchArgs.get())
   //   await expect(element(by.traits(['button'])).atIndex(1)).not.toBeVisible();
   // });
+  
+  // example tests
+  //   it('should have welcome screen', async () => {
+  //     await expect(element(by.id('welcome'))).toBeVisible();
+  //   });
 
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  //   it('should show hello screen after tap', async () => {
+  //     await element(by.id('hello_button')).tap();
+  //     await expect(element(by.text('Hello!!!'))).toBeVisible();
+  //   });
 
-  // it('should show world screen after tap', async () => {
-  //   await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('World!!!'))).toBeVisible();
-  // });
+  //   it('should show world screen after tap', async () => {
+  //     await element(by.id('world_button')).tap();
+  //     await expect(element(by.text('World!!!'))).toBeVisible();
+  //   });
+
 });
