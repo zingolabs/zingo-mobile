2cb4706d5fb1a62dc6ff7799755ece1df6b3138a
fluidvanadium <fluidvanadium@zingolabs.org>
2023-02-02 03:27:30 +0000

    hooked up DETOX, wrote basic test (pass & fail)

---
+++ e2e/my.test.js
@@ -0,0 +1,31 @@
+// const { log } = require('detox');
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have a visible button', async () => {
+    // log.error(device.appLaunchArgs.get())
+    await expect(element(by.traits(['button'])).atIndex(1)).toBeVisible();
+  });
+  
+  it('should actually be invisible button', async () => {
+    // log.error(device.appLaunchArgs.get())
+    await expect(element(by.traits(['button'])).atIndex(1)).not.toBeVisible();
+  });
+
+  // it('should show hello screen after tap', async () => {
+  //   await element(by.id('hello_button')).tap();
+  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+  // });
+
+  // it('should show world screen after tap', async () => {
+  //   await element(by.id('world_button')).tap();
+  //   await expect(element(by.text('World!!!'))).toBeVisible();
+  // });
+});
