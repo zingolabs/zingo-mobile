56fd66cd055dd47b9fe766b9547ad41f6a55784d
Hazel OHearn <gygaxis@zingolabs.org>
2023-02-10 15:39:49 -0400

    Move background sync to foreground task (in notification)

--- android/app/src/main/java/com/zingo/BackgroundSync.kt
+++ android/app/src/main/java/com/zingo/BackgroundSync.kt
@@ -1,11 +1,23 @@
 package org.ZingoLabs.Zingo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.jstasks.HeadlessJsTaskConfig
 
- class BackgroundSync : HeadlessJsTaskService() {
+
+class BackgroundSync : HeadlessJsTaskService() {
+    private val SERVICE_NOTIFICATION_ID = 12345;
+
     override fun getTaskConfig(intent: Intent): HeadlessJsTaskConfig? {
         return intent.extras?.let {
             HeadlessJsTaskConfig(
@@ -17,5 +29,47 @@ import com.facebook.react.jstasks.HeadlessJsTaskConfig
             )
         }
     }
+
+     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+         val channelId =
+             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                 createNotificationChannel()
+             } else {
+                 // If earlier version channel ID is not used
+                 // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
+                 ""
+             }
+
+         val notificationIntent = Intent(this, MainActivity::class.java)
+         val contentIntent = PendingIntent.getActivity(
+             this,
+             0,
+             notificationIntent,
+             PendingIntent.FLAG_CANCEL_CURRENT
+         )
+         val notification: Notification = NotificationCompat.Builder(this, channelId)
+             .setContentTitle("Zingo Background Sync")
+             .setContentText("Syncing...")
+             .setContentIntent(contentIntent)
+             .setOngoing(true)
+             .build()
+         startForeground(SERVICE_NOTIFICATION_ID, notification)
+         return super.onStartCommand(intent, flags, startId)
+     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String{
+        val channelId = "zbschannel"
+        val channelName = "Zingo Background Sync"
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
 }
+
 
