343e89c56ccf92eb86f373f99fdfec5502bdfb46
Hazel OHearn <gygaxis@zingolabs.org>
2023-02-13 14:43:34 -0400

    Some tweaks

--- android/app/src/main/java/org/ZingoLabs/Zingo/BackgroundSync.kt
+++ android/app/src/main/java/org/ZingoLabs/Zingo/BackgroundSync.kt
@@ -6,7 +6,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
