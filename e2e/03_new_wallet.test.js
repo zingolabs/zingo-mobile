8a14f2354f8dbfeea839b2680e020820cf7e0e2f
fluidvanadium <fluidvanadium@zingolabs.org>
2023-02-28 20:18:26 +0000

    some writeups, transitioning to multi-target build

---
+++ e2e/03_new_wallet.test.js
@@ -0,0 +1,28 @@
+const { log } = require('detox');
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  it('New wallet button works', async () => {
+    let newWalletButton = element(by.text('CREATE NEW WALLET (NEW SEED)'));
+    await newWalletButton.tap();
+    await element(by.text('I HAVE SAVED \n THE SEED')).tap();
+    // For some reason, the element that says 'ZEC', according to react,
+    // has text of ' ---'
+    // let zecText = element(by.text(" ---")).atIndex(1);
+    let text = element(by.id('transaction text'));
+    let attributes = await text.getAttributes();
+    log.info(attributes);
+    log.info('Value:', attributes.value);
+    log.info('Text:', attributes.text);
+    log.info('Visibility:', attributes.visibility);
+    log.info('Width (pixels):', attributes.width);
+
+    await expect(text).toBeVisible();
+    await expect(text).toHaveText('History');
+
+    //expect(element(by.traits(['button'])).filter())
+  });
+});
