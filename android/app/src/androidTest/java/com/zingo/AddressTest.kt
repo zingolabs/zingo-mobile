44e2f6fc8cabd31d1c1441276921c212eae330bf
Oscar Pepper <oscarpepper@zingolabs.org>
2023-03-02 10:44:47 -0600

    finished callAddress test

--- android/app/src/androidTest/java/com/zingo/AddressTest.kt
+++ android/app/src/androidTest/java/com/zingo/AddressTest.kt
@@ -2,6 +2,8 @@ package com.zingo
 
 import android.util.Base64
 import java.io.InputStream
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 //test dependencies
 import com.google.common.truth.Truth.assertThat
@@ -10,6 +12,17 @@ import org.junit.Test
 class RPCModule {
     private external fun execute(cmd: String, args: String): String
     private external fun initfromseed(serveruri: String, seed: String, birthday: String, saplingOutputb64: String, saplingSpendb64: String, datadir: String): String
+    
+    data class Addresses (
+    	val address : String,
+    	val receivers : Receivers
+    )
+
+    data class Receivers (
+    	val transparent : String,
+    	val sapling : String,
+    	val orchard_exists : Boolean
+    )
 
     @Test
     fun callAddress() {  
@@ -90,8 +103,12 @@ class RPCModule {
             saplingSpendEncoded.toString(),
             datadir)
     
+        // val addresses = execute("addresses", "")
         val resp = execute("addresses", "")
-        assertThat(resp).isEqualTo(1)
+        val mapper = jacksonObjectMapper()
+        val addresses: List<Addresses> = mapper.readValue(resp)
+        
+        assertThat(addresses[0].address).isEqualTo("u16sw4v6wy7f4jzdny55yzl020tp3yqg3c85dc6n7mmq0urfm6adqg79hxmyk85ufn4lun4pfh5q48cc3kvxhxm3w978eqqecdd260gkzjrkun6z7m9mcrt2zszaj0mvk6ufux2zteqwh57cq906hz3rkg63duaeqsvjelv9h5srct0zq8rvlv23wz5hed7zuatqd7p6p4ztugc4t4w2g")
     }
 }
 
