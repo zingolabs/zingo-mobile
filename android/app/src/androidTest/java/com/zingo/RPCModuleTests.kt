2dcfe8d1283e53adbbe6d79c226484a69d9d7a7c
Oscar Pepper <oscarpepper@zingolabs.org>
2023-03-02 11:44:04 -0600

    added sapling and transparent and renamed file

--- android/app/src/androidTest/java/com/zingo/RPCModuleTests.kt
+++ android/app/src/androidTest/java/com/zingo/RPCModuleTests.kt
@@ -25,7 +25,7 @@ class RPCModule {
     )
 
     @Test
-    fun callAddress() {  
+    fun executeAddresses() {  
         val server = "https://mainnet.lightwalletd.com:9067"
         val seed = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art"
         val birthday = "1"
@@ -98,17 +98,18 @@ class RPCModule {
 
         saplingOutput = ByteArray(0)
 
-        val rseed = initfromseed(server, seed, birthday,
+        initfromseed(server, seed, birthday,
             saplingOutputEncoded.toString(),
             saplingSpendEncoded.toString(),
             datadir)
     
-        // val addresses = execute("addresses", "")
         val resp = execute("addresses", "")
-        val mapper = jacksonObjectMapper()
-        val addresses: List<Addresses> = mapper.readValue(resp)
+        val addresses: List<Addresses> = jacksonObjectMapper().readValue(resp)
         
         assertThat(addresses[0].address).isEqualTo("u16sw4v6wy7f4jzdny55yzl020tp3yqg3c85dc6n7mmq0urfm6adqg79hxmyk85ufn4lun4pfh5q48cc3kvxhxm3w978eqqecdd260gkzjrkun6z7m9mcrt2zszaj0mvk6ufux2zteqwh57cq906hz3rkg63duaeqsvjelv9h5srct0zq8rvlv23wz5hed7zuatqd7p6p4ztugc4t4w2g")
+        assertThat(addresses[0].receivers.transparent).isEqualTo("t1dUDJ62ANtmebE8drFg7g2MWYwXHQ6Xu3F")
+        assertThat(addresses[0].receivers.sapling).isEqualTo("zs16uhd4mux24se6wkm74vld0ec63d4dxt3d7m80l5xytreplkkllrrf9c7fj859mhp8tkcq9hxfvj")
+        assertThat(addresses[0].receivers.orchard_exists).isEqualTo(true)
     }
 }
 
