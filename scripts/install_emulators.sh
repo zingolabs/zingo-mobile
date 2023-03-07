4c9f3394201a952a8b7a5e70332a234973a40617
fluidvanadium <fluidvanadium@zingolabs.org>
2023-03-07 03:28:39 +0000

    fix a typo

--- scripts/install_emulators.sh
+++ scripts/install_emulators.sh
@@ -3,7 +3,7 @@
 sdkmanager --install "system-images;android-29;google_apis_playstore;x86"
 sdkmanager --install "system-images;android-30;google_apis;x86_64"
 
-avdmanager create avd --force --name 29_gaps_32 --package 'system-images;android-29;google_apis_playstore;x86' -d pixel_6 --skin pixel_6
-avdmanager create avd --force --name 30_ga_64 --package 'system-images;android-30;google_apis;x86_64' -d pixel_2 --skin pixel_2
+avdmanager create avd --force --name 29_gaps_32 --package 'system-images;android-29;google_apis_playstore;x86' -d pixel_2 --skin pixel_2
+avdmanager create avd --force --name 30_ga_64 --package 'system-images;android-30;google_apis;x86_64' -d pixel_6 --skin pixel_6
 
 
