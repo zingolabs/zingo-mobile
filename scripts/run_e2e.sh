58af4fb41554318452f399fd2870dd8820c799b0
fluidvanadium <fluidvanadium@zingolabs.org>
2023-04-05 17:22:13 +0000

    upgraded run_e2e script to always bring react-native into the right context

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -1,3 +1,6 @@
+killall node
+nohup yarn react-native start > "out.yarn_react_native_start" &
+
 yarn detox build -c android.emu.64
 yarn detox test -c android.emu.64
 yarn detox build -c android.emu.32
