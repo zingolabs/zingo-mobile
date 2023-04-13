90525b8f9e956bf3ea0ce36b66f10046b7b114af
fluidvanadium <fluidvanadium@zingolabs.org>
2023-04-13 16:45:41 +0000

    polishes

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -3,5 +3,3 @@ nohup yarn react-native start > "out.yarn_react_native_start" &
 
 yarn detox build -c android.emu.64
 yarn detox test -c android.emu.64
-yarn detox build -c android.emu.32
-yarn detox test -c android.emu.32
