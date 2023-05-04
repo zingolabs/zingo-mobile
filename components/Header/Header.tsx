9c0d86914eba01f2e34f0e4a09e30e5dad519bd1
JC <juanky201271@gmail.com>
2023-05-04 13:54:07 -0600

    Fix: making the sync icons bigger in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -194,22 +194,22 @@ const Header: React.FunctionComponent<HeaderProps> = ({
                   borderColor: colors.primary,
                   borderWidth: 1,
                   borderRadius: 10,
-                  minWidth: 20,
-                  minHeight: 20,
+                  minWidth: 25,
+                  minHeight: 25,
                 }}>
                 {!syncStatusDisplayLine && syncingStatus.synced && (
                   <View style={{ margin: 0, padding: 0 }}>
-                    <FontAwesomeIcon icon={faCheck} color={colors.primary} />
+                    <FontAwesomeIcon icon={faCheck} color={colors.primary} size={20} />
                   </View>
                 )}
                 {!syncStatusDisplayLine && !syncingStatus.synced && (
                   <TouchableOpacity onPress={() => syncingStatusMoreInfoOnClick && syncingStatusMoreInfoOnClick()}>
-                    <FontAwesomeIcon icon={faStop} color={colors.zingo} size={12} />
+                    <FontAwesomeIcon icon={faStop} color={colors.zingo} size={17} />
                   </TouchableOpacity>
                 )}
                 {syncStatusDisplayLine && (
                   <TouchableOpacity onPress={() => syncingStatusMoreInfoOnClick && syncingStatusMoreInfoOnClick()}>
-                    <FontAwesomeIcon icon={faPlay} color={colors.primary} size={10} />
+                    <FontAwesomeIcon icon={faPlay} color={colors.primary} size={17} />
                   </TouchableOpacity>
                 )}
               </View>
