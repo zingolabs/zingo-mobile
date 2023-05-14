078c7f2c4bb030eddb3f974a6bab8a3db780965c
JC <juanky201271@gmail.com>
2023-05-13 20:12:11 -0600

    Fix: putting the new header

--- components/Insight/Insight.tsx
+++ components/Insight/Insight.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useContext } from 'react';
-import { View, ScrollView, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { View, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { PieChart } from 'react-native-svg-charts';
 import { Circle, G, Line, Text } from 'react-native-svg';
@@ -16,6 +16,7 @@ import { ThemeType } from '../../app/types';
 import { ContextAppLoaded } from '../../app/context';
 import Utils from '../../app/utils';
 import FadeText from '../Components/FadeText';
+import Header from '../Header';
 
 type InfoProps = {
   closeModal: () => void;
@@ -88,7 +89,7 @@ const pieData = data
 
 const Insight: React.FunctionComponent<InfoProps> = ({ closeModal }) => {
   const context = useContext(ContextAppLoaded);
-  const { info, totalBalance, translate } = context;
+  const { info, translate } = context;
   const { colors } = useTheme() as unknown as ThemeType;
 
   return (
@@ -100,30 +101,7 @@ const Insight: React.FunctionComponent<InfoProps> = ({ closeModal }) => {
         height: '100%',
         backgroundColor: colors.background,
       }}>
-      <View
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          paddingBottom: 10,
-          backgroundColor: colors.card,
-          zIndex: -1,
-          paddingTop: 10,
-        }}>
-        <Image
-          source={require('../../assets/img/logobig-zingo.png')}
-          style={{ width: 80, height: 80, resizeMode: 'contain' }}
-        />
-        <ZecAmount
-          currencyName={info.currencyName ? info.currencyName : ''}
-          size={36}
-          amtZec={totalBalance.total}
-          style={{ opacity: 0.5 }}
-        />
-        <RegText color={colors.money} style={{ marginTop: 5, padding: 5 }}>
-          {translate('insight.title') as string}
-        </RegText>
-        <View style={{ width: '100%', height: 1, backgroundColor: colors.primary }} />
-      </View>
+      <Header title={translate('insight.title') as string} noBalance={true} noSyncingStatus={true} noDrawMenu={true} />
 
       <ScrollView style={{ maxHeight: '85%' }} contentContainerStyle={{}}>
         <View style={{ display: 'flex', margin: 20, marginBottom: 30 }}>
