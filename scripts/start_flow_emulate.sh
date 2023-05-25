b29b4779eddd01a0a21479a1832eeb79483f07d6
fluidvanadium <fluidvanadium@zingolabs.org>
2023-05-25 22:05:49 +0000

    reordered for quick failure on invalid abi

--- scripts/start_flow_emulate.sh
+++ scripts/start_flow_emulate.sh
@@ -5,9 +5,9 @@ set -e
 #> docker
 #> sdkmanager (android command line tools, needs accept licenses)
 
-./scripts/build_apk.sh
-
 ./scripts/flow_emulator_setup.sh $@
+
+./scripts/build_apk.sh
 
 ./scripts/flow_emulator_run_apk.sh
 
