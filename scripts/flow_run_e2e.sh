a60c6358ce33522bf6e73769fa2efa265a6b9c23
fluidvanadium <fluidvanadium@zingolabs.org>
2023-06-02 18:45:55 +0000

    added ALL option to flow_run_e2e.sh

--- scripts/flow_run_e2e.sh
+++ scripts/flow_run_e2e.sh
@@ -3,9 +3,14 @@ set -e
 cd $(git rev-parse --show-toplevel)
 source ./scripts/emulator_read_target.sh
 
+test_suite=$1
+if [[ "${test_suite}" == "ALL" ]]; then
+  test_suite="";
+fi
+
 killall node
 nohup yarn react-native start &> "${output_dir}/react-native_start.txt" &
 
 detox_target="android.emu.${arch}"
 yarn detox build -c $detox_target
-yarn detox test -c $detox_target $@
+yarn detox test -c $detox_target $test_suite
