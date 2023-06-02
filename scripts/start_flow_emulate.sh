09239e81ea39b3a3378c2e452347b15f856d917b
fluidvanadium <fluidvanadium@zingolabs.org>
2023-06-02 18:20:31 +0000

    fine-tuned shortcut for interactive testing

--- scripts/start_flow_emulate.sh
+++ scripts/start_flow_emulate.sh
@@ -3,13 +3,12 @@ set -e
 
 cd $(git rev-parse --show-toplevel)
 
-#prerequisites:
-#> docker
-#> sdkmanager (android command line tools, needs accept licenses)
-
-./scripts/flow_emulator_setup.sh $@
+#extra prerequisites:
+#> emulator must be already installed
 
 ./scripts/build_apk.sh
 
-./scripts/flow_emulator_run_apk.sh
+./scripts/emulator_target.sh $@
+
+./scripts/flow_emulator_run_interactive.sh
 
