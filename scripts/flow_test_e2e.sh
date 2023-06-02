e6695e3fc1a9c18f88780b034158492f6bcf1fcc
fluidvanadium <fluidvanadium@zingolabs.org>
2023-06-02 21:47:40 +0000

    ci try 5

--- scripts/flow_test_e2e.sh
+++ scripts/flow_test_e2e.sh
@@ -11,15 +11,37 @@ if [[ "${test_suite}" == "ALL" ]]; then
 fi
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ./scripts/node_reboot.sh
 ||||||| parent of 4c39a0e1 (ci try 9)
 ./scripts/yarn_react-native_node_server.sh
 =======
+||||||| parent of 2927d61c (ci try 5)
+=======
+<<<<<<< HEAD
+>>>>>>> 2927d61c (ci try 5)
 echo 'going to react-native'
 ./scripts/yarn_react-native_node_server.sh
 echo 'ran react-native'
 >>>>>>> 4c39a0e1 (ci try 9)
 
+||||||| parent of dc09ca73 (ci try 5)
+echo 'messed with vars'
+
+killall node
+nohup yarn react-native start &> "${output_dir}/react-native_start.txt" &
+
+echo 'ran react-native'
+
+=======
+echo 'killing node'
+killall node
+
+echo 'yarn react-native start'
+nohup yarn react-native start &> "${output_dir}/react-native_start.txt" &
+
+echo 'ran'
+>>>>>>> dc09ca73 (ci try 5)
 detox_target="android.emu.${arch}"
 echo 'ready to detox'
 yarn detox build -c $detox_target
