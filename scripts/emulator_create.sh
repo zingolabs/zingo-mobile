3853c964657df5819085f6783af33ae446b165e0
fluidvanadium <fluidvanadium@zingolabs.org>
2023-06-06 22:57:31 +0000

    update print for mac

--- scripts/emulator_create.sh
+++ scripts/emulator_create.sh
@@ -6,5 +6,5 @@ cd $(git rev-parse --show-toplevel)
 source ./scripts/emulator_read_target.sh
 
 echo -e "\nCreating AVD..."
-avdmanager create avd --force --name "${avd_name}" --package "${sdk}" --device "${avd_device}" |& tee "${output_dir}/emulator_create.txt"
+avdmanager create avd --force --name "${avd_name}" --package "${sdk}" --device "${avd_device}" &> tee "${output_dir}/emulator_create.txt"
 
