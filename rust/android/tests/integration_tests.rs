76f9c94f5f95fae3a15c2efe70aaaaa4240acf3d
Oscar Pepper <oscarpepper@zingolabs.org>
2023-06-26 16:10:52 +0100

    cleaned up test and setup for symlink to regtest in zingolib dir

--- rust/android/tests/integration_tests.rs
+++ rust/android/tests/integration_tests.rs
@@ -3,37 +3,15 @@
 use env_logger;
 use zingo_testutils::{self, scenarios};
 
-// pub mod darkside;
-// use std::{fs::File, path::Path};
-// use bip0039::Mnemonic;
-// use data::seeds::HOSPITAL_MUSEUM_SEED;
-// use json::JsonValue::{self, Null};
-// use tokio::time::Instant;
-// use tracing_test::traced_test;
-// use zcash_address::unified::Ufvk;
-// use zcash_client_backend::encoding::encode_payment_address;
-// use zcash_primitives::{
-//     consensus::Parameters,
-//     transaction::{components::amount::DEFAULT_FEE, TxId},
-// };
-// use zingo_testutils::regtest::get_cargo_manifest_dir_parent;
-// use zingoconfig::{ChainType, ZingoConfig};
-// use zingolib::{
-//     check_client_balances, get_base_address,
-//     lightclient::LightClient,
-//     wallet::{
-//         data::TransactionMetadata,
-//         keys::{
-//             extended_transparent::ExtendedPrivKey,
-//             unified::{Capability, WalletCapability},
-//         },
-//         LightWallet, Pool, WalletBase,
-//     },
-// };
-
 #[tokio::test]
 async fn execute_address() {
     env_logger::init();
-    let (_regtest_manager, _child_process_handler, _lightclient) =
+    let (_regtest_manager, child_process_handler, lightclient) =
         scenarios::basic_no_spendable().await;
+
+    let _addresses = dbg!(lightclient.do_addresses().await);
+    assert_eq!(1, 2);
+
+    // Unneeded, but more explicit than having child_process_handler be an unused variable
+    drop(child_process_handler);
 }
