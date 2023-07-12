0dd4587c899df858fd2ff8549de2ffd25f496da2
Oscar Pepper <oscarpepper@zingolabs.org>
2023-07-12 17:35:53 +0100

    added mobile regtest scenario to integ test

--- rust/android/tests/integration_tests.rs
+++ rust/android/tests/integration_tests.rs
@@ -6,12 +6,7 @@ use zingo_testutils::{self, scenarios};
 #[tokio::test]
 async fn execute_address() {
     env_logger::init();
-    let (_regtest_manager, child_process_handler, lightclient) =
-        scenarios::basic_no_spendable().await;
+    let (_regtest_manager, child_process_handler) = scenarios::mobile_regtest_poc().await;
 
-    let _addresses = dbg!(lightclient.do_addresses().await);
-    assert_eq!(1, 2);
-
-    // Unneeded, but more explicit than having child_process_handler be an unused variable
     drop(child_process_handler);
 }
