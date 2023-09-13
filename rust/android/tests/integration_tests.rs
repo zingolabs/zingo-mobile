eeef0e744a0da872a3aa2daefabf94ab0bacc489
Oscar Pepper <oscarpepper@zingolabs.org>
2023-09-13 11:19:45 +0100

    added cargo cache and matched integ test modules to ABI names for CI

--- rust/android/tests/integration_tests.rs
+++ rust/android/tests/integration_tests.rs
@@ -101,7 +101,7 @@ mod x86_64 {
     }
 }
 
-mod arm32 {
+mod armeabi-v7a {
     const ABI: &str = "armeabi-v7a";
 
     #[tokio::test]
@@ -120,7 +120,7 @@ mod arm32 {
     }
 }
 
-mod arm64 {
+mod arm64-v8a {
     const ABI: &str = "arm64-v8a";
 
     #[tokio::test]
