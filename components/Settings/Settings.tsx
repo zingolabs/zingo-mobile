e602859f02dd58ed34749a1a0e177bc2fd1ac246
JC <juanky201271@gmail.com>
2023-12-23 01:24:03 -0700

    fix: debug mode option only for advanced users

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -694,7 +694,7 @@ const Settings: React.FunctionComponent<SettingsProps> = ({
               <BoldText>{translate('settings.memo-title') as string}</BoldText>
             </View>
 
-            <View style={{ display: 'flex', marginLeft: 25, marginBottom: 30 }}>
+            <View style={{ display: 'flex', marginLeft: 25 }}>
               {optionsRadio(
                 MEMOS,
                 setMemos as React.Dispatch<React.SetStateAction<string | boolean>>,
@@ -703,21 +703,22 @@ const Settings: React.FunctionComponent<SettingsProps> = ({
                 'memo',
               )}
             </View>
+
+            <View style={{ display: 'flex', margin: 10 }}>
+              <BoldText>{translate('settings.debugmode-title') as string}</BoldText>
+            </View>
+
+            <View style={{ display: 'flex', marginLeft: 25, marginBottom: 30 }}>
+              {optionsRadio(
+                DEBUGMODES,
+                setDebugMode as React.Dispatch<React.SetStateAction<string | boolean>>,
+                Boolean,
+                debugMode,
+                'debugmode',
+              )}
+            </View>
           </>
         )}
-        <View style={{ display: 'flex', margin: 10 }}>
-          <BoldText>{translate('settings.debugmode-title') as string}</BoldText>
-        </View>
-
-        <View style={{ display: 'flex', marginLeft: 25 }}>
-          {optionsRadio(
-            DEBUGMODES,
-            setDebugMode as React.Dispatch<React.SetStateAction<string | boolean>>,
-            Boolean,
-            debugMode,
-            'debugmode',
-          )}
-        </View>
       </ScrollView>
       <View
         style={{
