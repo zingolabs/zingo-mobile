9455cc68bd6bcc44ac2d9e6556d3dd501b4fbcbc
JC <juanky201271@gmail.com>
2024-01-21 14:54:03 -0700

    fix: integration test - testing sh script more 12

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -351,18 +351,9 @@ else
 
     echo -e "\nTest reports saved: android/${test_report_dir}"
     
-    word="FAILURES"
-    count_fails=$(cat "${test_report_dir}/test_results.txt" | grep -ow "$(echo ${word})" | wc -w)
-    trimmed_count_fails=$(echo "${count_fails}" | tr -d ' ')
-    
-    #failss=$(cat "${test_report_dir}/test_results.txt" | grep -ow "FAILURES" | wc -w)
-    #trimmed_failss=$(echo "${failss}" | tr -d ' ')
-    
-    echo "fails: ${trimmed_count_fails}"
-    
     code=$(cat "${test_report_dir}/test_results.txt" | grep INSTRUMENTATION_CODE: | cut -d' ' -f2 | tr -d ' ')
     echo "code: ${code}"
-    if [[ "${code}" -ne "-1" || "${trimmed_count_fails}" -ne "0" ]]; then
+    if [[ "${code}" -ne "-1" || $(cat "${test_report_dir}/test_results.txt" | grep -q "FAILURES!!!") ]]; then
         echo -e "\nIntegration tests FAILED"
 
         # Kill all emulators
