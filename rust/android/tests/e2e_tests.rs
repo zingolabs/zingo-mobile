db61435945736c7cf1c7e7e60a3960a2d7cf84f3
Oscar Pepper <oscarpepper@zingolabs.org>
2024-02-07 10:15:35 +0000

    improved output to show blocks synced in background cont.

--- rust/android/tests/e2e_tests.rs
+++ rust/android/tests/e2e_tests.rs
@@ -99,8 +99,8 @@ mod e2e {
             }
             #[tokio::test]
             async fn background_sync_benchmark_test() {
-                const BLOCKCHAIN_HEIGHT: u64 = 1_000;
-                // const BLOCKCHAIN_HEIGHT: u64 = 90_000;
+                // const BLOCKCHAIN_HEIGHT: u64 = 1_000;
+                const BLOCKCHAIN_HEIGHT: u64 = 90_000;
                 let transaction_set = load_chainbuild_file("background_sync_benchmark");
                 let mut scenario = DarksideScenario::new(Some(20_000)).await;
                 scenario.build_faucet(Pool::Orchard).await;
