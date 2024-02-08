f2887a01d4a046a9b30f452460c43197d7f067d9
Hazel OHearn <gygaxis@zingolabs.org>
2024-02-08 17:20:44 -0400

    work in progress no lose-y

--- android/app/src/main/java/org/ZingoLabs/Zingo/MainActivity.kt
+++ android/app/src/main/java/org/ZingoLabs/Zingo/MainActivity.kt
@@ -23,55 +23,11 @@ class MainActivity : ReactActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.w("", "Starting main activity")
-        val service = Intent(applicationContext, BackgroundSync::class.java)
-        service.setAction(STOP)
-        applicationContext.stopService(service)
         super.onCreate(null)
     }
 
     override fun onPause() {
-        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Log.w("", "Creating notification channel")
-            createNotificationChannel()
-        } else {
-            // If earlier version channel ID is not used
-            // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
-            ""
-        }
         Log.w("", "Pausing main activity")
-        val service = Intent(applicationContext, BackgroundSync::class.java)
-        service.setAction(START)
-        val bundle = Bundle()
-
-        bundle.putString("channelId", channelId)
-        bundle.putInt("notifId", 12345)
-       ;
-
-        service.putExtras(bundle)
-
-        applicationContext.startService(service)
         super.onPause()
-        //val backgroundRequest = PeriodicWorkRequest.Builder(BackgroundWorker::class.java, 15, TimeUnit.MINUTES).build()
-        //WorkManager.getInstance(application).enqueue(backgroundRequest)
-    }
-
-    override fun onResume() {
-        val service = Intent(applicationContext, BackgroundSync::class.java)
-        service.setAction(STOP)
-        applicationContext.stopService(service)
-        super.onResume()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(): String{
-        val channelId = "zbschannel"
-        val channelName = "Zingo Background Sync"
-        val chan = NotificationChannel(channelId,
-            channelName, NotificationManager.IMPORTANCE_DEFAULT)
-        chan.lightColor = Color.BLUE
-        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
-        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        service.createNotificationChannel(chan)
-        return channelId
     }
 }
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import BackgroundSync from './app/BackgroundSync';
+import notifee from '@notifee/react-native';
 
-AppRegistry.registerHeadlessTask('BackgroundSync', () => BackgroundSync);
+notifee.registerForegroundService(BackgroundSync);
 AppRegistry.registerComponent(appName, () => App);
