5d9ce2af0facc0d1e35eeee17bcdc23b78d176a9
JC <juanky201271@gmail.com>
2024-02-17 15:28:03 -0700

    fix: adopting the same calculation from ecc for timeDifference

--- android/app/src/main/java/org/ZingoLabs/Zingo/MainActivity.kt
+++ android/app/src/main/java/org/ZingoLabs/Zingo/MainActivity.kt
@@ -1,11 +1,27 @@
 package org.ZingoLabs.Zingo
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.work.*
 import com.facebook.react.ReactActivity
-import java.util.concurrent.TimeUnit
-import java.util.Calendar
+import kotlin.random.Random
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+import kotlin.time.toJavaDuration
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.until
 
 class MainActivity : ReactActivity() {
     /**
@@ -13,6 +29,10 @@ class MainActivity : ReactActivity() {
      * rendering of the component.
      */
     private val taskID = "Zingo_Processing_Task_ID"
+    private val SYNC_PERIOD = 24.hours
+    private val SYNC_DAY_SHIFT = 1.days // Move to tomorrow
+    private val SYNC_START_TIME_HOURS = 3.hours // Start around 3 a.m. at night
+    private val SYNC_START_TIME_MINUTES = 60.minutes // Randomize with minutes until 4 a.m.
     override fun getMainComponentName(): String {
         return "Zingo!"
     }
@@ -23,12 +43,14 @@ class MainActivity : ReactActivity() {
         super.onCreate(null)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onPause() {
         Log.w("ON_PAUSE", "Pausing main activity - Background")
         scheduleBackgroundTask()
         super.onPause()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         Log.w("ON_RESUME", "Resuming main activity - Foreground")
         // cancel the task if it is in execution now
@@ -43,6 +65,7 @@ class MainActivity : ReactActivity() {
             .cancelUniqueWork(taskID)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun scheduleBackgroundTask() {
         val constraints = Constraints.Builder()
             .setRequiresStorageNotLow(true)
@@ -51,23 +74,14 @@ class MainActivity : ReactActivity() {
             .build()
 
         // PRODUCTION - next day between 3:00 and 4:00 am.
-        val currentTime = Calendar.getInstance()
-        val targetTime = Calendar.getInstance().apply {
-            set(Calendar.HOUR_OF_DAY, 3)
-            set(Calendar.MINUTE, (0..59).random())
-            set(Calendar.SECOND, 0)
-            if (before(currentTime)) {
-                add(Calendar.DATE, 1)
-            }
-        }
-        val timeDifference = targetTime.timeInMillis - currentTime.timeInMillis
+        val targetTimeDiff = calculateTargetTimeDifference()
 
         // DEVELOPMENT - after 5 minutes.
         //val timeFiveMinutes: Long = 5
 
-        val workRequest = PeriodicWorkRequest.Builder(BackgroundSyncWorker::class.java, 24, TimeUnit.HOURS)
+        val workRequest = PeriodicWorkRequest.Builder(BackgroundSyncWorker::class.java, SYNC_PERIOD.toJavaDuration())
             .setConstraints(constraints)
-            .setInitialDelay(timeDifference, TimeUnit.MILLISECONDS)
+            .setInitialDelay(targetTimeDiff.toJavaDuration())
             //.setInitialDelay(timeFiveMinutes, TimeUnit.MINUTES)
             .build()
 
@@ -75,8 +89,36 @@ class MainActivity : ReactActivity() {
         WorkManager.getInstance(this)
             .enqueueUniquePeriodicWork(
                 taskID,
-                ExistingPeriodicWorkPolicy.REPLACE,
+                ExistingPeriodicWorkPolicy.KEEP,
                 workRequest
             )
     }
+
+    private fun calculateTargetTimeDifference(): Duration {
+            val currentTimeZone: TimeZone = TimeZone.currentSystemDefault()
+
+            val now: Instant = Clock.System.now()
+
+            val targetTime =
+                now
+                    .plus(SYNC_DAY_SHIFT)
+                    .toLocalDateTime(currentTimeZone)
+                    .date
+                    .atTime(
+                        hour = SYNC_START_TIME_HOURS.inWholeHours.toInt(),
+                        // Even though the WorkManager will trigger the work approximately at the set time, it's
+                        // better to randomize time in 3-4 a.m. This generates a number between 0 (inclusive) and 60
+                        // (exclusive)
+                        minute = Random.nextInt(0, SYNC_START_TIME_MINUTES.inWholeMinutes.toInt())
+                    )
+
+            val targetTimeTime = targetTime.time
+            Log.w("SCHEDULING_TASK", "calculated target time $targetTimeTime")
+
+            return now.until(
+                other = targetTime.toInstant(currentTimeZone),
+                unit = DateTimeUnit.MILLISECOND,
+                timeZone = currentTimeZone
+            ).toDuration(DurationUnit.MILLISECONDS)
+        }
 }
