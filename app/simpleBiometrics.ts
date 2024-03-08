691cfbb0ec2149bf01723165d21bbee1b4261861
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2024-03-08 14:25:38 -0700

    fix: passcode working with no biometric set up

--- app/simpleBiometrics.ts
+++ app/simpleBiometrics.ts
@@ -1,34 +1,22 @@
 import { Platform } from 'react-native';
 import RNBiometrics from 'react-native-simple-biometrics';
 import { TranslateType } from './AppState';
-import simplePasscode from './simplePasscode';
 
 type simpleBiometricsProps = {
   translate: (key: string) => TranslateType;
 };
 
 const simpleBiometrics = async (props: simpleBiometricsProps) => {
-  // Check if biometric authentication is available
-  const can = await RNBiometrics.canAuthenticate();
-
-  if (can) {
-    try {
-      await RNBiometrics.requestBioAuth(
-        props.translate('biometrics-title') as string,
-        props.translate('biometrics-message') as string,
-      );
-      // Code to execute when authenticated
-      return true;
-    } catch (error) {
-      // Code to handle authentication failure
-      return false;
-    }
-  }
-  // if the biometric autehtication is not available for IOS
-  // I'll try with the Passcode authentication
-  // only for IOS
-  if (Platform.OS === 'ios') {
-    return await simplePasscode({ translate: props.translate });
+  try {
+    await RNBiometrics.requestBioAuth(
+      props.translate('biometrics-title') as string,
+      props.translate('biometrics-message') as string,
+    );
+    // Code to execute when authenticated
+    return true;
+  } catch (error) {
+    // Code to handle authentication failure
+    return false;
   }
 };
 
