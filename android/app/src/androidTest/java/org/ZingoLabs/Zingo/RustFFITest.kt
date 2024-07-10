75b6aa253569e7e14f3a13805aa2ed5da5433d2f
JC <juanky201271@gmail.com>
2024-07-10 09:33:24 -0600

    fix: android value transfers test fixed

--- android/app/src/androidTest/java/org/ZingoLabs/Zingo/RustFFITest.kt
+++ android/app/src/androidTest/java/org/ZingoLabs/Zingo/RustFFITest.kt
@@ -346,7 +346,7 @@ class UpdateCurrentPriceAndValueTransfersFromSeed {
         // the value transfers have 3 items for 3 different txs
         // 1. Received - 1_000_000 - orchard (1 item)
         // 2. Sent - 110_000 - uregtest1zkuzfv5m3... (1 item)
-        // 3. memoToSelf - 10_000 (1 item) -> in valueTransfers doesn't exists (Why?)
+        // 3. sendToSelf - 10_000 (1 item) -> in valueTransfers doesn't exists (Why?)
         assertThat(valueTranfers.value_transfers.size).isEqualTo(3)
         // first item have to be a `Received`
         assertThat(valueTranfers.value_transfers[0].kind).isEqualTo("received")
@@ -360,7 +360,7 @@ class UpdateCurrentPriceAndValueTransfersFromSeed {
         assertThat(valueTranfers.value_transfers[1].value).isEqualTo(100000)
         assertThat(valueTranfers.value_transfers[1].transaction_fee).isEqualTo(10000)
         // third item have to be a `fee` from the last `Sent` with the same txid
-        assertThat(valueTranfers.value_transfers[2].kind).isEqualTo("memo-to-self")
+        assertThat(valueTranfers.value_transfers[2].kind).isEqualTo("send-to-self")
         assertThat(valueTranfers.value_transfers[2].status).isEqualTo("confirmed")
         assertThat(valueTranfers.value_transfers[2].value).isEqualTo(100000)
         assertThat(valueTranfers.value_transfers[2].transaction_fee).isEqualTo(10000)
