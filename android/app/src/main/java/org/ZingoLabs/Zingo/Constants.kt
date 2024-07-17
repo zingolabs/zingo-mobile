bf3b2e6e88e7a6cc35fd31dc31e2aa37ac8230ee
JC <juanky201271@gmail.com>
2024-07-17 15:57:28 -0600

    fix: kotlin layer refactored

---
+++ android/app/src/main/java/org/ZingoLabs/Zingo/Constants.kt
@@ -0,0 +1,10 @@
+package org.ZingoLabs.Zingo
+
+enum class Constants(val value: String) {
+    // wallet files
+    WalletFileName("wallet.dat"),
+    WalletBackupFileName("wallet.backup.dat"),
+
+    BackgroundFileName("background.json"),
+    ErrorPrefix("error"),
+}
