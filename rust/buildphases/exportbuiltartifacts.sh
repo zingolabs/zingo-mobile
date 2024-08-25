fe26ea45cfcdb1ad9499440b0eb047481171ad8a
JC <juanky201271@gmail.com>
2024-08-25 11:13:20 -0600

    fix: kotlin uniffi build in ./build.sh script

--- rust/buildphases/exportbuiltartifacts.sh
+++ rust/buildphases/exportbuiltartifacts.sh
@@ -6,6 +6,9 @@ mkdir -p ../android/app/src/main/jniLibs/armeabi-v7a
 mkdir -p ../android/app/src/main/jniLibs/x86
 mkdir -p ../android/app/src/main/jniLibs/x86_64
 
+mkdir -p ../android/app/build/generated/source/uniffi/debug/java/uniffi/zingo
+mkdir -p ../android/app/build/generated/source/uniffi/release/java/uniffi/zingo
+
 id=$(docker create devlocal/build_android)
 
 docker cp \
@@ -20,5 +23,12 @@ docker cp \
 docker cp \
     $id:/opt/zingo/rust/target/aarch64-linux-android/release/libzingo.so \
     ../android/app/src/main/jniLibs/arm64-v8a/libuniffi_zingo.so
+
+docker cp \
+    $id:/opt/zingo/rust/lib/src/uniffi/zingo/zingo.kt \
+    ../android/app/build/generated/source/uniffi/debug/java/uniffi/zingo/zingo.kt
+docker cp \
+    $id:/opt/zingo/rust/lib/src/uniffi/zingo/zingo.kt \
+    ../android/app/build/generated/source/uniffi/release/java/uniffi/zingo/zingo.kt
 
 docker rm -v $id
