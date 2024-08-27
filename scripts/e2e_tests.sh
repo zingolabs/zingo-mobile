2d802fcec5a57bfff05d3f365636b2beda1b02fb
JC <juanky201271@gmail.com>
2024-08-26 18:27:22 -0600

    fix: adb wait-for-device added for e2e - fix

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -239,8 +239,7 @@ if [[ $create_snapshot == true ]]; then
     echo no | avdmanager create avd --force --name "${avd_name}" --package "${sdk}" --device "${device}"
 
     echo -e "\n\nWaiting for emulator to launch & boot..."
-    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -gpu off -no-boot-anim \
-        -no-snapshot-load -port 5554 &> /dev/null &
+    nohup emulator -avd "${avd_name}" -gpu off -no-boot-anim -no-snapshot-load -port 5554 &> /dev/null &
     adb wait-for-device
     echo "$(adb devices | grep "emulator-5554" | cut -f1) launch successful"
 
@@ -268,8 +267,7 @@ else
     mkdir -p "${test_report_dir}"
 
     echo -e "\n\nWaiting for emulator to launch & boot..."
-    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -gpu off -no-boot-anim \
-        -no-snapshot-save -read-only -port 5554 &> "${test_report_dir}/emulator.txt" &
+    nohup emulator -avd "${avd_name}" -gpu off -no-boot-anim -no-snapshot-load -port 5554 &> /dev/null &
     adb wait-for-device
     echo "$(adb devices | grep "emulator-5554" | cut -f1) launch successful"
 
