dcb14f2a5fd88ed8228119d1142c13ea56520455
JC <juanky201271@gmail.com>
2024-08-27 17:28:10 -0600

    fix: using x86_64 34 and x86 30 in CI google atd - no input service

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -280,7 +280,7 @@ else
     sleep 1
 
     # Disable animations
-    adb shell input keyevent 82
+    #adb shell input keyevent 82
     adb shell settings put global window_animation_scale 0.0
     adb shell settings put global transition_animation_scale 0.0
     adb shell settings put global animator_duration_scale 0.0
