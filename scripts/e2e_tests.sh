ebed82f2da1692ca525db7b82328317b0a8f353c
JC <juanky201271@gmail.com>
2024-08-28 12:17:07 -0600

    fix: avd emulator with -gpu off

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -239,7 +239,7 @@ if [[ $create_snapshot == true ]]; then
     echo no | avdmanager create avd --force --name "${avd_name}" --package "${sdk}" --device "${device}"
 
     echo -e "\n\nWaiting for emulator to launch & boot..."
-    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -no-window -no-audio -gpu swiftshader_indirect -no-boot-anim \
+    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -no-window -no-audio -gpu off -no-boot-anim \
         -no-snapshot-load -port 5554 &> /dev/null &
     adb wait-for-device
     echo "$(adb devices | grep "emulator-5554" | cut -f1) launch successful"
@@ -272,7 +272,7 @@ else
     mkdir -p "${test_report_dir}"
 
     echo -e "\n\nWaiting for emulator to launch & boot..."
-    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -no-window -no-audio -gpu swiftshader_indirect -no-boot-anim \
+    nohup emulator -avd "${avd_name}" -netdelay none -netspeed full -no-window -no-audio -gpu off -no-boot-anim \
         -no-snapshot-save -read-only -port 5554 &> "${test_report_dir}/emulator.txt" &
     adb wait-for-device
     echo "$(adb devices | grep "emulator-5554" | cut -f1) launch successful"
