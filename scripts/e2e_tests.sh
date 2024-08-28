088f5859f2e3c6173ac796dc5c79e28f90dc3e4e
JC <juanky201271@gmail.com>
2024-08-27 20:09:37 -0600

    fix: avoiding use emulator services

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -281,9 +281,9 @@ else
 
     # Disable animations
     #adb shell input keyevent 82
-    adb shell settings put global window_animation_scale 0.0
-    adb shell settings put global transition_animation_scale 0.0
-    adb shell settings put global animator_duration_scale 0.0
+    #adb shell settings put global window_animation_scale 0.0
+    #adb shell settings put global transition_animation_scale 0.0
+    #adb shell settings put global animator_duration_scale 0.0
 
     echo -e "\nInstalling APKs..."
     i=0
