45a61b7e22efe4529aae345ad64eef24b414d552
sebastianlujan <sebastianlujan@proton.me>
2024-09-24 22:24:19 -0300

    feat: Migrate build configuration from goovy DSL to Kotlin DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,33 +1,30 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext {
-        buildToolsVersion = "34.0.0"
-        minSdkVersion = 24
-        compileSdkVersion = 34
-        targetSdkVersion = 34
-        ndkVersion = "23.2.8568313"
-        kotlinVersion = '1.9.0'
-    }
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath('com.android.tools.build:gradle:8.1.1')
-        classpath("com.facebook.react:react-native-gradle-plugin")
-	    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-    }
+  val kotlinVersion = "1.9.0"
+  val buildToolsVersion = "34.0.0"
+  val minSdkVersion = 24
+  val compileSdkVersion = 34
+  val targetSdkVersion = 34
+  val ndkVersion = "23.2.8568313"
+
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:8.1.1")
+    classpath("com.facebook.react:react-native-gradle-plugin")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+  }
 }
 
 allprojects {
-   repositories {
-     google()
-     maven {
-       url("$rootDir/../node_modules/detox/Detox-android")
-     }
-     maven { url 'https://www.jitpack.io' }
-   }
+  repositories {
+    google()
+    maven("$rootDir/../node_modules/detox/Detox-android")
+    maven("https://www.jitpack.io")
+  }
  }
 
 // this is not the best solution, but while the libraries have no namespace...
