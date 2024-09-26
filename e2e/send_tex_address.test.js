72cbea511b0dcc52dc78f6bfdc218b9f5f375cb8
dariovp <dariovp01@gmail.com>
2024-09-26 13:37:59 -0300

    test: correct timings

--- e2e/send_tex_address.test.js
+++ e2e/send_tex_address.test.js
@@ -9,17 +9,21 @@ describe('Renders wallet data correctly.', () => {
   it('parses the TEX address and correctly renders the confirm screen', async () => {
     await waitFor(element(by.id('vt-1')))
       .toExist()
-      .withTimeout(20000);
+      .withTimeout(30000);
     await element(by.text('SEND')).tap();
 
     // Address taken from the reference implementation
     await element(by.id('send.addressplaceholder')).replaceText('tex1s2rt77ggv6q989lr49rkgzmh5slsksa9khdgte');
 
-    await expect(element(by.text('Invalid Address!'))).not.toExist();
+    await waitFor(element(by.text('Invalid Address!')))
+      .not.toExist()
+      .withTimeout(2000);
 
     await element(by.id('send.amount')).replaceText('0');
     await element(by.id('send.scroll-view')).scrollTo('bottom');
-    await waitFor(element(by.id('send.button'))).toBeVisible();
+    await waitFor(element(by.id('send.button')))
+      .toBeVisible()
+      .withTimeout(5000);
     await element(by.id('send.button')).tap();
 
     await expect(element(by.id('send.confirm.scroll-view'))).toExist();
