43324ddafd96549ac325075d7f8a1c37696f2590
JC <juanky201271@gmail.com>
2024-10-03 07:23:08 -0600

    fix: trying api 33 with x86_64

--- rust/zingomobile_utils/src/lib.rs
+++ rust/zingomobile_utils/src/lib.rs
@@ -63,26 +63,12 @@ pub fn android_integration_test_ci(abi: &str, test_name: &str) -> (i32, String, 
         arg = "/C".to_string();
     }
 
-    #[cfg(not(any(target_arch = "arm", target_arch = "aarch64")))]
     let output = Command::new(command)
         .arg(arg)
         .arg(format!(
             r#"
             cd $(git rev-parse --show-toplevel)
             ./scripts/ci/integration_tests_ci.sh -a {} -e {}
-            "#,
-            abi, test_name
-        ))
-        .output()
-        .expect("Failed to execute command");
-
-    #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
-    let output = Command::new(command)
-        .arg(arg)
-        .arg(format!(
-            r#"
-            cd $(git rev-parse --show-toplevel)
-            ./scripts/ci/integration_tests_ci.sh -a {} -e {} -A
             "#,
             abi, test_name
         ))
@@ -159,26 +145,12 @@ pub fn android_e2e_test_ci(abi: &str, test_name: &str) -> (i32, String, String) 
         arg = "/C".to_string();
     }
 
-    #[cfg(not(any(target_arch = "arm", target_arch = "aarch64")))]
     let output = Command::new(command)
         .arg(arg)
         .arg(format!(
             r#"
             cd $(git rev-parse --show-toplevel)
             ./scripts/ci/e2e_tests_ci.sh -a {} -e {}
-            "#,
-            abi, test_name
-        ))
-        .output()
-        .expect("Failed to execute command");
-
-    #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
-    let output = Command::new(command)
-        .arg(arg)
-        .arg(format!(
-            r#"
-            cd $(git rev-parse --show-toplevel)
-            ./scripts/ci/e2e_tests_ci.sh -a {} -e {} -A
             "#,
             abi, test_name
         ))
