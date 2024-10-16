b9fb2eeb5daf72a4c52193be5a7ae9eb020a7a11
JC <juanky201271@gmail.com>
2024-10-16 17:15:21 -0600

    fix: fix & ci e2e new test added

--- e2e/parse_invalid_address.test.js
+++ e2e/parse_invalid_address.test.js
@@ -5,15 +5,15 @@ import { loadRecipientWallet } from './e2e-utils/loadRecipientWallet.js';
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 describe('Renders wallet data correctly.', () => {
-  beforeEach(async () => {
-    await device.launchApp({
-      newInstance: true,
-    });
-  });
   it('loads a wallet', async () => await loadRecipientWallet());
   it('does not parse an incorrect address', async () => {
+    await waitFor(element(by.id('vt-1')))
+    .toExist()
+    .withTimeout(30000);
     await element(by.text('SEND')).tap();
+
     await element(by.id('send.addressplaceholder')).replaceText('thisisaninvalidaddress');
+
     await waitFor(element(by.id('send.address.error')))
       .toExist()
       .withTimeout(5000);
