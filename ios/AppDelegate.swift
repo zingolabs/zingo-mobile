6ef41ba0c5c9a9cdd9d19d4adf817887b1b4ee1c
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2025-01-26 08:29:37 -0700

    fix: IOS - upgrade RN fixes

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -24,7 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     let jsCodeLocation: URL
 
-    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackExtension: nil)
+    //jsCodeLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle")!
     let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "Zingo", initialProperties: nil, launchOptions: launchOptions)
     let rootViewController = UIViewController()
     rootViewController.view = rootView
