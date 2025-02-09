9a30ff1f93da3336ea94895ff0a2ba89671953ea
dorianvp <dariovp01@gmail.com>
2025-02-08 23:58:31 -0300

    chore(deps): Bump react-native to 0.74.0

--- android/app/src/main/java/org/ZingoLabs/Zingo/MainApplication.kt
+++ android/app/src/main/java/org/ZingoLabs/Zingo/MainApplication.kt
@@ -1,6 +1,7 @@
 package org.ZingoLabs.Zingo
 
 import android.app.Application
+import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -10,39 +11,39 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import android.content.Context
 import java.lang.ref.WeakReference
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-              add(RPCPackage())
+            object : DefaultReactNativeHost(this) {
+                override fun getPackages(): List<ReactPackage> =
+                        PackageList(this).packages.apply {
+                            // Packages that cannot be autolinked yet can be added manually here,
+                            // for example:
+                            // add(MyReactNativePackage())
+                            add(RPCPackage())
+                        }
+
+                override fun getJSMainModuleName(): String = "index"
+
+                override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+                override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+                override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
             }
- 
-        override fun getJSMainModuleName(): String = "index"
- 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
- 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-    }
- 
+
     override val reactHost: ReactHost
-        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
- 
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
     override fun onCreate() {
         super.onCreate()
         context = WeakReference(applicationContext)
 
-
         SoLoader.init(this, false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            // If you opted-in for the New Architecture, we load the native entry point for this
+            // app.
             load()
         }
     }
@@ -58,4 +59,3 @@ class MainApplication : Application(), ReactApplication {
         }
     }
 }
-
