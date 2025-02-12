1688f7cdb0d4ef2012453b579d69e3485f92f997
dorianvp <dariovp01@gmail.com>
2025-02-12 01:47:19 -0300

    tests(scanner): Update Jest snapshot file for `react-native-vision-camera`

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,22 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+
+/**
+ * See https://react-native-vision-camera.com/docs/guides/mocking for more information.
+ */
+const config = {
+  resolver: {
+    ...defaultResolver,
+    sourceExts: [process.env.RN_SRC_EXT && process.env.RN_SRC_EXT.split(','), ...defaultResolver.sourceExts],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
