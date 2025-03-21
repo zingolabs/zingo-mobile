f9398639586fc937fdc1994ffe4eeb8e9404dfc7
JC <juanky201271@gmail.com>
2025-03-20 19:06:52 -0600

    fix: adjusting script 2

--- rust/build_fdroid.sh
+++ rust/build_fdroid.sh
@@ -149,7 +149,7 @@ cp ../target/x86_64-linux-android/release/libzingo.so /opt/jniLibs/x86_64/libuni
 cp ../target/i686-linux-android/release/libzingo.so /opt/jniLibs/x86/libuniffi_zingo.so
 cp ../target/armv7-linux-androideabi/release/libzingo.so /opt/jniLibs/armeabi-v7a/libuniffi_zingo.so
 cp ../target/aarch64-linux-android/release/libzingo.so /opt/jniLibs/arm64-v8a/libuniffi_zingo.so
-cp ./src/uniffi/zingo/zingo.kt /opt/jniLibs/zingo.kt
+cp ./src/uniffi/zingo/zingo.kt /opt/jniLibs/kotlin/zingo.kt
 
 rm -rf ../target
 rm -rf /opt/openssl-3.3.2
