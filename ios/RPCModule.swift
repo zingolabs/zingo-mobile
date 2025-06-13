ef082f035a21fb9355ab0c094f1c1956a6b625b7
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2025-06-13 16:33:04 -0600

    fix: file size log for iOS

--- ios/RPCModule.swift
+++ ios/RPCModule.swift
@@ -194,6 +194,8 @@ class RPCModule: NSObject {
   func saveWalletInternal() throws {
     let walletEncodedString = saveToB64()
     if !walletEncodedString.lowercased().hasPrefix(Constants.ErrorPrefix.rawValue) {
+      let size = (walletEncodedString.count * 3) / 4
+      NSLog("file size \(size)")
       try self.saveWalletFile(walletEncodedString)
     } else {
       throw FileError.saveFileError("Couldn't save the wallet. \(walletEncodedString)")
