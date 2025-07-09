d219abf4e3e628aafa39b1f17ccae3a1f00cbcf2
JC <juanky201271@gmail.com>
2025-07-09 23:36:09 +0200

    fix: change sendall by send_all

--- app/AppState/enums/CommandEnum.ts
+++ app/AppState/enums/CommandEnum.ts
@@ -1,7 +1,7 @@
 export enum CommandEnum {
   // new commands to create a proposal
   send = 'send',
-  sendall = 'sendall',
+  sendall = 'send_all',
   shield = 'shield',
   // this works for all: send, sendall & shield
   confirm = 'confirm',
