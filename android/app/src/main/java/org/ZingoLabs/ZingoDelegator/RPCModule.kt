bd699681e7e9466f872f44073f7f0116927c7021
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2025-11-11 09:45:33 -0700

    fix: kotlin reference fix

--- android/app/src/main/java/org/ZingoLabs/ZingoDelegator/RPCModule.kt
+++ android/app/src/main/java/org/ZingoLabs/ZingoDelegator/RPCModule.kt
@@ -10,7 +10,7 @@ import com.facebook.react.bridge.Promise
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
-import org.ZingoLabs.Zingo.Constants.*
+import org.ZingoLabs.ZingoDelegator.Constants.*
 import kotlinx.coroutines.*
 
 class RPCModule internal constructor(private val reactContext: ReactApplicationContext?) : ReactContextBaseJavaModule(reactContext) {
