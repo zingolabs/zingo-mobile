dce70316b505d579534aa1a716de447c070dc3c9
dorianvp <dorianvp@zingolabs.org>
2025-11-12 22:09:40 -0300

    chore(`dockerfile`): fix ios build script

--- rust/ios/build.sh
+++ rust/ios/build.sh
@@ -3,14 +3,13 @@
 # not working on xcode
 #export RUSTFLAGS="-C debuginfo=1 -C split-debuginfo=off -C symbol-mangling-version=v0"
 
-ln -s $(which node) /usr/local/bin/node
+set -euo pipefail
 
 cargo install --force --locked bindgen-cli
 
 cd ../lib
-cargo run --release --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
-cargo build --release --target aarch64-apple-ios -Z build-std
-cargo build --release --target x86_64-apple-ios -Z build-std
+cargo run --features="uniffi/cli" --release --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
+cargo build --release --target aarch64-apple-ios --target x86_64-apple-ios
 cargo lipo --release --targets aarch64-apple-ios x86_64-apple-ios
 
 cp ./Generated/zingo.swift ../../ios
