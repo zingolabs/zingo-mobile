098800f749382720589722696db549c770c7a662
dorianvp <dorianvp@zingolabs.org>
2025-11-13 23:30:57 -0300

    chore(`dockerfile`): more ios fixes

--- rust/ios/buildsimulator.sh
+++ rust/ios/buildsimulator.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-ln -s $(which node) /usr/local/bin/node
+set -euo pipefail
+
+# Pick the iOS Simulator SDK and tools. This is necessary for librocksdb-sys compilation
+SDK_SIMULATOR="$(xcrun --sdk iphonesimulator --show-sdk-path)"
+CLANG_SIMULATOR="$(xcrun --sdk iphonesimulator -f clang)"
+AR_SIMULATOR="$(xcrun --sdk iphonesimulator -f ar)"
+
+export BINDGEN_EXTRA_CLANG_ARGS_aarch64_apple_ios_sim="--target=arm64-apple-ios-simulator -isysroot ${SDK_SIMULATOR}"
+export BINDGEN_EXTRA_CLANG_ARGS_x86_64_apple_ios="-isysroot ${SDK_SIMULATOR}"
+
+# Use the simulator toolchain for C builds under those Rust targets
+export CC_aarch64_apple_ios_sim="${CLANG_SIMULATOR}"
+export AR_aarch64_apple_ios_sim="${AR_SIMULATOR}"
+export CC_x86_64_apple_ios="${CLANG_SIMULATOR}"
+export AR_x86_64_apple_ios="${AR_SIMULATOR}"
 
 cargo install --force --locked bindgen-cli
 
 cd ../lib
-cargo run --release --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
-cargo build --release --target aarch64-apple-ios-sim -Z build-std
-cargo build --release --target x86_64-apple-ios -Z build-std
+cargo run --release --features="uniffi/cli" --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
+cargo build --release --target aarch64-apple-ios-sim --target x86_64-apple-ios
 cargo lipo --release --targets aarch64-apple-ios-sim x86_64-apple-ios
 
 cp ./Generated/zingo.swift ../../ios
