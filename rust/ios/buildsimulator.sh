522fe55d5d7b460937cd35fc5eacd2341f3ac29a
dorianvp <dorianvp@zingolabs.org>
2025-11-15 17:54:10 -0300

    chore(`dockerfile`): remove x86 for ios

--- rust/ios/buildsimulator.sh
+++ rust/ios/buildsimulator.sh
@@ -13,13 +13,15 @@ export BINDGEN_EXTRA_CLANG_ARGS_x86_64_apple_ios="-isysroot ${SDK_SIMULATOR}"
 # Use the simulator toolchain for C builds under those Rust targets
 export CC_aarch64_apple_ios_sim="${CLANG_SIMULATOR}"
 export AR_aarch64_apple_ios_sim="${AR_SIMULATOR}"
-export CC_x86_64_apple_ios="${CLANG_SIMULATOR}"
-export AR_x86_64_apple_ios="${AR_SIMULATOR}"
+
+# If using an Intel Mac
+# export CC_x86_64_apple_ios="${CLANG_SIMULATOR}"
+# export AR_x86_64_apple_ios="${AR_SIMULATOR}"
 
 cd ../lib
 cargo run --release --features="uniffi/cli" --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
-cargo build --release --target aarch64-apple-ios-sim --target x86_64-apple-ios
-cargo lipo --release --targets aarch64-apple-ios-sim x86_64-apple-ios
+cargo build --release --target aarch64-apple-ios-sim
+cargo lipo --release --targets aarch64-apple-ios-sim
 
 cp ./Generated/zingo.swift ../../ios
 cp ./Generated/zingoFFI.h ../../ios
