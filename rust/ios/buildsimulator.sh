bd274b7700e00a80b4528d59c053143dd37dbc69
dorianvp <dorianvp@zingolabs.org>
2025-11-15 16:40:05 -0300

    chore(`dockerfile`): reapply librocksdb support

--- rust/ios/buildsimulator.sh
+++ rust/ios/buildsimulator.sh
@@ -2,6 +2,20 @@
 
 set -euo pipefail
 
+# Pick the iOS Simulator SDK and tools. This is necessary for librocksdb-sys compilation
+SDK_SIMULATOR="$(xcrun --sdk iphonesimulator --show-sdk-path)"
+CLANG_SIMULATOR="$(xcrun --sdk iphonesimulator -f clang)"
+AR_SIMULATOR="$(xcrun --sdk iphonesimulator -f ar)"
+
+export BINDGEN_EXTRA_CLANG_ARGS_aarch64_apple_ios_sim="--target=arm64-apple-ios-simulator -isysroot ${SDK_SIMULATOR}"
+export BINDGEN_EXTRA_CLANG_ARGS_x86_64_apple_ios="-isysroot ${SDK_SIMULATOR}"
+
+# Use the simulator toolchain for C builds under those Rust targets
+export CC_aarch64_apple_ios_sim="${CLANG_SIMULATOR}"
+export AR_aarch64_apple_ios_sim="${AR_SIMULATOR}"
+export CC_x86_64_apple_ios="${CLANG_SIMULATOR}"
+export AR_x86_64_apple_ios="${AR_SIMULATOR}"
+
 cd ../lib
 cargo run --release --features="uniffi/cli" --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
 cargo build --release --target aarch64-apple-ios-sim --target x86_64-apple-ios
