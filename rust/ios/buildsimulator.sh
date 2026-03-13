a16335502c7b4831dd5e5c0d827689de43a1a675
JC <juanky201271@gmail.com>
2026-03-13 10:03:13 -0600

    fix: CI - rust nightly to stable - add targets

--- rust/ios/buildsimulator.sh
+++ rust/ios/buildsimulator.sh
@@ -2,12 +2,14 @@
 
 ln -s $(which node) /usr/local/bin/node
 
+rustup default stable
+
 cargo install --force --locked bindgen-cli
 
 cd ../lib
 cargo run --release --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
-cargo build --release --target aarch64-apple-ios-sim -Z build-std
-cargo build --release --target x86_64-apple-ios -Z build-std
+cargo build --release --target aarch64-apple-ios-sim
+cargo build --release --target x86_64-apple-ios
 cargo lipo --release --targets aarch64-apple-ios-sim x86_64-apple-ios
 
 cp ./Generated/zingo.swift ../../ios
