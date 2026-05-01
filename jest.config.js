785a2a49a814424257c3d1fa6457a93f0a5d6bea
JC <juanky201271@gmail.com>
2026-05-01 13:34:29 -0600

    fix: CI coverage added

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,11 @@ module.exports = {
   },
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageReporters: ['lcov', 'text'],
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '!**/__tests__/**',
+    '!**/node_modules/**',
+  ],
 };
