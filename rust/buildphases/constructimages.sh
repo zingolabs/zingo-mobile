f50ea1a61a249bc1f7ad66336ccff3cae5722e74
JC <juanky201271@gmail.com>
2026-05-11 15:16:23 -0600

    fix: initial work

---
+++ rust/buildphases/constructimages.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+# build image
+podman build --target build_android --tag devlocal/build_android .
--- rust/buildphases/local/exportbuiltartifacts.sh
+++ rust/buildphases/local/exportbuiltartifacts.sh
@@ -9,25 +9,25 @@ mkdir -p ../android/app/src/main/jniLibs/x86_64
 mkdir -p ../android/app/build/generated/source/uniffi/debug/java/uniffi/zingo
 mkdir -p ../android/app/build/generated/source/uniffi/release/java/uniffi/zingo
 
-id=$(docker create devlocal/build_android)
+id=$(podman create devlocal/build_android)
 
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/target/x86_64-linux-android/release/libzingo.so \
     ../android/app/src/main/jniLibs/x86_64/libuniffi_zingo.so
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/target/i686-linux-android/release/libzingo.so \
     ../android/app/src/main/jniLibs/x86/libuniffi_zingo.so
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/target/armv7-linux-androideabi/release/libzingo.so \
     ../android/app/src/main/jniLibs/armeabi-v7a/libuniffi_zingo.so
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/target/aarch64-linux-android/release/libzingo.so \
     ../android/app/src/main/jniLibs/arm64-v8a/libuniffi_zingo.so
 
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/lib/src/uniffi/zingo/zingo.kt \
     ../android/app/build/generated/source/uniffi/debug/java/uniffi/zingo/zingo.kt
-docker cp \
+podman cp \
     $id:/opt/zingo/rust/lib/src/uniffi/zingo/zingo.kt \
     ../android/app/build/generated/source/uniffi/release/java/uniffi/zingo/zingo.kt
-docker rm -v $id
+podman rm -v $id
