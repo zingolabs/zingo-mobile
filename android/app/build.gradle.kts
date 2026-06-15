cd1ee4fb075b83e230fb1d31a103707d329451c7
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2026-06-15 09:51:46 -0600

    release: zingo beta 2.0.21 (313)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -154,7 +154,7 @@ android {
         create("beta") {
             dimension = "channel"
             applicationIdSuffix = ".Beta"
-            versionCode = 312 // beta override
+            versionCode = 313 // beta override
             versionName = "2.0.21" // beta override
             resValue("string", "app_name", "Zingo Beta")
         }
