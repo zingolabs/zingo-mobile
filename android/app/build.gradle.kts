55c20c386181b7cfbd34180f7b6046e8243b86b7
Juanky <juanky201271@gmail.com>
2026-09-05 11:18:32 -0600

    release: zingo beta 2.0.23 (329)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -162,7 +162,7 @@ android {
         create("beta") {
             dimension = "channel"
             applicationIdSuffix = ".Beta"
-            versionCode = 328 // beta override
+            versionCode = 329 // beta override
             versionName = "2.0.23" // beta override
             resValue("string", "app_name", "Zingo Beta")
             resValue("bool", "enforce_privacy_controls", "false")
