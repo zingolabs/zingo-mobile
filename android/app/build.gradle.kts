8000dc88e667d7230b0697b1a731a08d9420dd6f
Juanky <juanky201271@gmail.com>
2026-09-23 09:55:49 -0600

    release: zingo 2.0.24 (320)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -121,7 +121,7 @@ android {
         applicationId = "org.ZingoLabs.Zingo" // Real
         minSdk = rootProject.extra["minSdkVersion"] as Int
         targetSdk = rootProject.extra["targetSdkVersion"] as Int
-        versionCode = 319 // Real (prod baseline; beta flavor overrides below)
+        versionCode = 320 // Real (prod baseline; beta flavor overrides below)
         versionName = "2.0.24" // Real
         testBuildType = System.getProperty("testBuildType", "debug")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
